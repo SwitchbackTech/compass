30fd1fa2d9134be7a9cf59834128b89acb8b3147
Ty Hitzeman <ty@switchback.tech>
2022-01-21 19:45:49 -0600

    reverted back to local host

--- packages/web/src/common/constants/api.ts
+++ packages/web/src/common/constants/api.ts
@@ -1,4 +1,4 @@
 // TODO change once you put the production web server and
 // backend on separate VMs
-// export const BASEURL = "http://localhost:3000";
-export const BASEURL = "https://***REMOVED***";
+export const BASEURL = "http://localhost:3000";
+// export const BASEURL = "https://***REMOVED***";
