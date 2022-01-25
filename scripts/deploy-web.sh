dcd8a5ed9e74450f0306eb2a251a23adcb741f98
Ty Hitzeman <ty@switchback.tech>
2022-01-25 12:33:07 -0600

    wip: deploy script

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -5,4 +5,4 @@
 rm -r /compass-calendar/prod/build.bak # remove old backup build
 mv /compass-calendar/prod/build /compass-calendar/prod/build.bak # create new backup
 unzip -n -d /compass-calendar/prod/ /home/***REMOVED***/build.zip
-mv /home/***REMOVED***/build.zip /home/***REMOVED***/build.zip.bak # TODO just delete this one
+rm /home/***REMOVED***/build.zip # already have a backup now, so delete this one
