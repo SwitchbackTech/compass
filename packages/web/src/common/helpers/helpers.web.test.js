8b375fbaf3cbaf6a1666bfa36c0abcf0b4f421f5
Ty Hitzeman <ty@switchback.tech>
2022-02-02 20:18:07 -0600

    fix: tz test

--- packages/web/src/common/helpers/helpers.web.test.js
+++ packages/web/src/common/helpers/helpers.web.test.js
@@ -1,13 +1,4 @@
 import { headers, convertTimeZones } from "@web/common/helpers";
-// import { convertTimeZones } from './index';
-
-describe("timezone stuff", () => {
-  it("works idk temp change", () => {
-    const orig = "2022-02-01T11:15:00-06:00";
-    const updated = convertTimeZones(orig, "America/Los_Angeles");
-    const y = 1;
-  });
-});
 
 describe("headers", () => {
   it("uses Bearer token", () => {
