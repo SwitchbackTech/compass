52beab91505b0099f91a9cbd48f13eac34170068
Ty Hitzeman <ty@switchback.tech>
2022-02-15 15:12:27 -0600

    feat: update Login test

--- packages/web/src/views/Login/Login.test.tsx
+++ packages/web/src/views/Login/Login.test.tsx
@@ -4,18 +4,20 @@ import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import { LoginView } from "@web/views/Login";
 
-it("renders google oauth button", () => {
+it("renders oauth and feedback buttons", () => {
   render(<LoginView />);
   const gOauthButton = screen.getByRole("button", {
     name: /connect my google calendar/i,
   });
-
   expect(gOauthButton).toBeInTheDocument;
+
+  const feedbackButton = screen.getByText(/send feedback/i);
+  expect(feedbackButton).toBeInTheDocument;
 });
 
 it("shows loading message after clicking auth button", async () => {
   window.open = jest.fn();
-  const { getByText } = render(<LoginView />);
+  render(<LoginView />);
   const user = userEvent.setup();
   const gOauthButton = screen.getByRole("button", {
     name: /connect my google calendar/i,
