d25a4a7c1529368414d82211af97691f5912ecd1
Ty Hitzeman <ty@switchback.tech>
2022-02-16 12:33:50 -0600

    feat: added tests to CalendarView

--- packages/web/src/views/Calendar/Calendar.test.tsx
+++ packages/web/src/views/Calendar/Calendar.test.tsx
@@ -14,25 +14,70 @@ beforeAll(() => {
   window.HTMLElement.prototype.scroll = jest.fn();
 });
 
-describe("CalendarView: First Render", () => {
+describe("CalendarView: Renders", () => {
   beforeEach(() => {
     render(<CalendarView />);
   });
-  it("renders current year in YYYY format", () => {
+  it("current year in YYYY format", () => {
     const currentYear = new Date().getFullYear().toString(); // YYYY
     expect(screen.getByText(currentYear)).toBeInTheDocument;
   });
 
-  it("renders navigation arrows", () => {
+  it("navigation arrows", () => {
     expect(screen.getByText(/</i)).toBeInTheDocument;
     expect(screen.getByText(/>/i)).toBeInTheDocument;
   });
-  it("renders current week", () => {
+  it("current week", () => {
     const todaysDate = new Date().getDate().toString();
     expect(todaysDate).toBeInTheDocument;
   });
-  it("scrolls", () => {
+  it("automatically scrolls", () => {
     expect(window.HTMLElement.prototype.scroll).toHaveBeenCalled;
+  });
+  it("timed and all day event", () => {
+    const preloadedState = {
+      events: {
+        getWeekEvents: {
+          value: {
+            data: ["620c177bfadfdec705cdd70a", "620c177bfadfdec705cdd69c"],
+          },
+        },
+        entities: {
+          value: {
+            "620c177bfadfdec705cdd70a": {
+              _id: "620c177bfadfdec705cdd70a",
+              gEventId: "pihjll1k75s1g9019ru6tkb97c",
+              user: "61f2f0704d0ee49134c7a01d",
+              origin: "googleimport",
+              title: "groceries",
+              description: "foo",
+              priorities: [],
+              isAllDay: false,
+              startDate: "2022-02-14T11:45:00-06:00",
+              endDate: "2022-02-14T12:45:00-06:00",
+              priority: "relations",
+            },
+            "620c177bfadfdec705cdd69c": {
+              _id: "620c177bfadfdec705cdd69c",
+              gEventId:
+                "6csjad336cs3ibb469imcb9kc9gj6bb26ss30bb56kojgoj464o66ohh60",
+              user: "61f2f0704d0ee49134c7a01d",
+              origin: "googleimport",
+              title: "chill all day",
+              description: "just chillin",
+              priorities: [],
+              isAllDay: true,
+              startDate: "2022-02-16",
+              endDate: "2022-02-17",
+              priority: "relations",
+            },
+          },
+        },
+      },
+    };
+    render(<CalendarView />, { preloadedState });
+    expect(screen.getByText("groceries")).toBeInTheDocument();
+    expect(screen.getByText("chill all day")).toBeInTheDocument();
   });
 });
 
@@ -69,7 +114,7 @@ describe("CalendarView: Interactions", () => {
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
 
-  it("renders previous week upon nav arrow click", async () => {
+  it("navigates to previous week upon nav arrow click", async () => {
     const user = userEvent.setup();
     const { container } = render(<CalendarView />);
     const todayId = "#id-" + getWeekDayLabel(dayjs());
