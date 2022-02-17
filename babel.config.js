57a3d7bd250b0fd17556a17ecce07c9d66fb5024
Ty Hitzeman <ty@switchback.tech>
2022-02-17 17:27:50 -0600

    feat: added babel plugin for styled-com debugging

--- babel.config.js
+++ babel.config.js
@@ -4,4 +4,11 @@ module.exports = {
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript",
   ],
+  plugins: [
+    "babel-plugin-styled-components",
+    {
+      meaninglessFileNames: ["index", "styled"],
+      pure: true,
+    },
+  ],
 };
