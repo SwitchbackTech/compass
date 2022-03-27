aa19c826bddc47c235751881d2a3ce67d6b09f8d
Ty Hitzeman <ty@switchback.tech>
2022-03-27 06:32:37 -0600

    refactor: dev collection names

--- packages/backend/src/common/constants/collections.ts
+++ packages/backend/src/common/constants/collections.ts
@@ -4,10 +4,10 @@ import { isDev } from "../helpers/common.helpers";
 const useDevCollections = isDev();
 
 export const Collections = {
-  CALENDARLIST: useDevCollections ? "dev.calendarlist" : "calendarlist",
-  WATCHLOG_GCAL: useDevCollections ? "dev.watch.gcal" : "watch.gcal",
-  EVENT: useDevCollections ? "dev.event" : "event",
-  OAUTH: useDevCollections ? "dev.oauth" : "oauth",
-  PRIORITY: useDevCollections ? "dev.priority" : "priority",
-  USER: useDevCollections ? "dev.user" : "user",
+  CALENDARLIST: useDevCollections ? "_dev.calendarlist" : "calendarlist",
+  WATCHLOG_GCAL: useDevCollections ? "_dev.watch.gcal" : "watch.gcal",
+  EVENT: useDevCollections ? "_dev.event" : "event",
+  OAUTH: useDevCollections ? "_dev.oauth" : "oauth",
+  PRIORITY: useDevCollections ? "_dev.priority" : "priority",
+  USER: useDevCollections ? "_dev.user" : "user",
 };
