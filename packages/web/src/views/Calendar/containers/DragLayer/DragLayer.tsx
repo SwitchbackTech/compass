b7c34a56bcbfacbe709f0dc0b9c87f31e259f815
Ty Hitzeman <ty@switchback.tech>
2022-05-11 06:41:19 -0500

    refactor: remove DragLayer demo

--- packages/web/src/views/Calendar/containers/DragLayer/DragLayer.tsx
+++ packages/web/src/views/Calendar/containers/DragLayer/DragLayer.tsx
@@ -1,38 +1,10 @@
 import React, { FC, memo } from "react";
-import type { XYCoord } from "react-dnd";
 import { useDragLayer } from "react-dnd";
 import { DragItem, DropResult } from "@web/common/types/dnd.types";
 import { WeekViewProps } from "@web/views/Calendar/weekViewHooks/useGetWeekViewProps";
 
 import { DraggableEvent } from "./DraggableEvent";
-import { layerStyles } from "./styled";
-import { snapToGrid } from "./snap.grid";
-
-function getItemStyles(
-  initialOffset: XYCoord | null,
-  currentOffset: XYCoord | null
-) {
-  if (!initialOffset || !currentOffset) {
-    return {
-      display: "none",
-    };
-  }
-
-  let { x, y } = currentOffset;
-
-  // snap logic
-  x -= initialOffset.x;
-  y -= initialOffset.y;
-  [x, y] = snapToGrid(x, y);
-  x += initialOffset.x;
-  y += initialOffset.y;
-
-  const transform = `translate(${x}px, ${y}px)`;
-  return {
-    transform,
-    WebkitTransform: transform,
-  };
-}
+import { getItemStyles, layerStyles } from "./styled";
 
 export interface Props {
   snapToGrid?: boolean;
--- packages/web/src/views/Calendar/containers/DragLayer/styled.ts
+++ packages/web/src/views/Calendar/containers/DragLayer/styled.ts
@@ -1,8 +1,37 @@
 import styled from "styled-components";
+import type { XYCoord } from "react-dnd";
 import type { CSSProperties } from "react";
 import { Priority } from "@core/core.constants";
 import { hoverColorsByPriority } from "@web/common/utils/colors";
 import { ZIndex } from "@web/common/constants/web.constants";
+
+import { snapToGrid } from "./snap.grid";
+
+export const getItemStyles = (
+  initialOffset: XYCoord | null,
+  currentOffset: XYCoord | null
+) => {
+  if (!initialOffset || !currentOffset) {
+    return {
+      display: "none",
+    };
+  }
+
+  let { x, y } = currentOffset;
+
+  // snap logic
+  x -= initialOffset.x;
+  y -= initialOffset.y;
+  [x, y] = snapToGrid(x, y);
+  x += initialOffset.x;
+  y += initialOffset.y;
+
+  const transform = `translate(${x}px, ${y}px)`;
+  return {
+    transform,
+    WebkitTransform: transform,
+  };
+};
 
 export const layerStyles: CSSProperties = {
   position: "fixed",
