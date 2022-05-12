fc2341fd3e2c909758a18d244dc854982f10c058
Ty Hitzeman <ty@switchback.tech>
2022-05-12 13:27:46 -0500

    refactor: customRender arrow func

--- packages/web/src/common/__mocks__/simple.mock.render.tsx
+++ packages/web/src/common/__mocks__/simple.mock.render.tsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, PreloadedState } from "@reduxjs/toolkit";
 import { render as rtlRender } from "@testing-library/react";
 import { reducers } from "@web/store/reducers";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-function render(
+const customRender = (
   ui: JSX.Element,
   {
     preloadedState,
     store = configureStore({ reducer: reducers, preloadedState }),
     ...renderOptions
   } = {}
-) {
-  function Wrapper({ children }) {
+) => {
+  const AllTheProviders = ({ children }) => {
     return (
       <DndProvider backend={HTML5Backend}>
         <Provider store={store}>{children}</Provider>;
       </DndProvider>
     );
-  }
-  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
-}
+  };
+  return rtlRender(ui, { wrapper: AllTheProviders, ...renderOptions });
+};
 
 // re-export everything
 export * from "@testing-library/react";
 // override render method
-export { render };
+export { customRender as render };
