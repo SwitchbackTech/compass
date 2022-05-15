0f76736a908f1e2137ca4beb171a141e15e36bfc
Ty Hitzeman <ty@switchback.tech>
2022-05-15 16:39:40 -0500

    fix: Login test mock

--- packages/web/src/views/Login/Login.test.tsx
+++ packages/web/src/views/Login/Login.test.tsx
@@ -1,34 +1,61 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 import { act, render, screen, waitFor } from "@testing-library/react";
 import { LoginView } from "@web/views/Login";
+import { API_BASEURL } from "@web/common/constants/web.constants";
 
-it("renders oauth and feedback buttons", async () => {
-  render(<LoginView />);
-  const gOauthButton = screen.getByRole("button", {
-    name: /sign up/i,
+describe("Login Tests", () => {
+  // move to setup file
+  const server = setupServer(
+    rest.get(`${API_BASEURL}/auth/oauth-url`, (req, res, ctx) => {
+      return res(ctx.json({ authUrl: "foo", authState: "bar" }));
+    }),
+    rest.get("/api/auth/oauth-status", (req, res, ctx) => {
+      return res(
+        ctx.json({ isOauthComplete: false, refreshNeeded: false, token: "noo" })
+      );
+    })
+  );
+
+  beforeAll(() => {
+    server.listen();
   });
-  await waitFor(() => {
-    expect(gOauthButton).toBeInTheDocument;
-  });
-
-  const feedbackButton = screen.getByText(/send feedback/i);
-  expect(feedbackButton).toBeInTheDocument;
-});
-
-it("shows loading message after clicking auth button", async () => {
-  window.open = jest.fn();
-  render(<LoginView />);
-  const user = userEvent.setup();
-
-  const gOauthButton = screen.getByRole("button", {
-    name: /sign up/i,
-  });
-  await act(async () => {
-    await user.click(gOauthButton);
+  afterAll(() => {
+    server.close();
   });
 
-  const loadingEl = screen.getByRole("heading", { name: /loading \.\.\./i });
-  expect(loadingEl).toBeInTheDocument();
+  it("renders oauth and feedback buttons", async () => {
+    render(<LoginView />);
+    const gOauthButton = screen.getByRole("button", {
+      name: /sign up/i,
+    });
+    await waitFor(() => {
+      expect(gOauthButton).toBeInTheDocument;
+    });
+
+    const feedbackButton = screen.getByText(/send feedback/i);
+    expect(feedbackButton).toBeInTheDocument;
+  });
+
+  it("shows loading message after clicking auth button", async () => {
+    window.open = jest.fn();
+
+    render(<LoginView />);
+    const user = userEvent.setup();
+
+    const gOauthButton = screen.getByRole("button", {
+      name: /sign up/i,
+    });
+    await act(async () => {
+      await user.click(gOauthButton);
+    });
+
+    const loadingEl = screen.getByRole("heading", { name: /loading \.\.\./i });
+    await waitFor(() => {
+      expect(loadingEl).toBeInTheDocument();
+    });
+  });
 });
