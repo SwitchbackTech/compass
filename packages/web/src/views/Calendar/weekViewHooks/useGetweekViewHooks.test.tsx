5c25714d85b17c11ba5419f950ede62e530fb694
Ty Hitzeman <ty@switchback.tech>
2022-05-15 15:31:38 -0500

    fix: act warnings in all other tests.

--- packages/web/src/views/Calendar/weekViewHooks/useGetweekViewHooks.test.tsx
+++ packages/web/src/views/Calendar/weekViewHooks/useGetweekViewHooks.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react";
 import { reducers } from "@web/store/reducers";
 
 import { useGetWeekViewProps } from "./useGetWeekViewProps";
--- packages/web/src/views/Login/Login.test.tsx
+++ packages/web/src/views/Login/Login.test.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen, waitFor } from "@testing-library/react";
 import { LoginView } from "@web/views/Login";
 
-it("renders oauth and feedback buttons", () => {
+it("renders oauth and feedback buttons", async () => {
   render(<LoginView />);
   const gOauthButton = screen.getByRole("button", {
     name: /sign up/i,
   });
-  expect(gOauthButton).toBeInTheDocument;
+  await waitFor(() => {
+    expect(gOauthButton).toBeInTheDocument;
+  });
 
   const feedbackButton = screen.getByText(/send feedback/i);
   expect(feedbackButton).toBeInTheDocument;
@@ -19,10 +21,14 @@ it("shows loading message after clicking auth button", async () => {
   window.open = jest.fn();
   render(<LoginView />);
   const user = userEvent.setup();
+
   const gOauthButton = screen.getByRole("button", {
     name: /sign up/i,
   });
-  await user.click(gOauthButton);
+  await act(async () => {
+    await user.click(gOauthButton);
+  });
+
   const loadingEl = screen.getByRole("heading", { name: /loading \.\.\./i });
   expect(loadingEl).toBeInTheDocument();
 });
