3b5b0f502d8e0071e4419522c7acf76502ebabf4
Ty Hitzeman <ty@switchback.tech>
2023-01-04 09:56:59 -0600

    -

--- packages/web/src/views/Login/styled.ts
+++ packages/web/src/views/Login/styled.ts
@@ -4,18 +4,18 @@ import { getColor } from "@core/util/color.utils";
 import { ColorNames } from "@core/types/color.types";
 
 export const StyledLogin = styled(Flex)`
+  align-items: center;
   background: ${getColor(ColorNames.BLUE_2)};
   bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   left: 0;
+  min-height: 100vh;
   position: fixed;
   right: 0;
   text-align: center;
   top: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
 `;
 
 export const GoogleBtnWrapper = styled.div`
