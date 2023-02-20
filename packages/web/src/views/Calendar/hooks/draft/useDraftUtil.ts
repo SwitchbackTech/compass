5d30adc716f24e6c95c4d9be1e2c783d00e4eb1d
Ty Hitzeman <ty@switchback.tech>
2023-02-20 12:31:55 -0600

    refactor: preventDefault inside useGridMouseMove

--- packages/web/src/views/Calendar/hooks/draft/useDraftUtil.ts
+++ packages/web/src/views/Calendar/hooks/draft/useDraftUtil.ts
@@ -155,8 +155,6 @@ export const useDraftUtil = (
   const drag = useCallback(
     (e: MouseEvent) => {
       const updateTimesDuringDrag = (e: MouseEvent) => {
-        e.preventDefault();
-
         setDraft((_draft) => {
           const x = getX(e, isSidebarOpen);
           const _initialStart = dateCalcs.getDateByXY(
