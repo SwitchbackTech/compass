157d567fa9b23d6313e27aafa24be0349e85cb2f
Ty Hitzeman <ty@switchback.tech>
2023-04-02 11:41:53 -0500

    cleanup: getPosition

--- packages/web/src/ducks/events/types/someday.slice.types.ts
+++ packages/web/src/ducks/events/types/someday.slice.types.ts
@@ -1,5 +1,4 @@
 import { Action } from "redux";
-import { Schema_SomedayEventsColumn } from "@web/common/types/web.event.types";
 
 export interface Action_Someday_Reorder extends Action {
   payload: Payload_Someday_Reorder[];
@@ -8,12 +7,4 @@ export interface Action_Someday_Reorder extends Action {
 interface Payload_Someday_Reorder {
   _id: string;
   order: number;
-}
-
-//++
-interface _Payload_Someday_Reorder {
-  origIndex: number;
-  newIndex: number;
-  reorderedEvent: string;
-  somedayEvents: Schema_SomedayEventsColumn;
 }
