d1115ee74c185867dbb28fdb3ce4dcb4933d5e5f
Tyler Dane <tyler@switchback.tech>
2024-01-23 14:21:45 -0600

    :recycle: Refactor(auth-protection): create ProtectedRoute

--- packages/web/src/routers/index.tsx
+++ packages/web/src/routers/index.tsx
@@ -1,18 +1,41 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React, { ReactNode, useEffect } from "react";
+import Session from "supertokens-auth-react/recipe/session";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
 import { ROOT_ROUTES } from "@web/common/constants/routes";
 import { LoginView } from "@web/views/Login";
-import { Calendar } from "@web/views/Calendar/Calendar";
 import { LogoutView } from "@web/views/Logout";
 import { NotFoundView } from "@web/views/NotFound";
+import { CalendarView } from "@web/views/Calendar";
+
+export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchSession() {
+      const isAuthenticated = await Session.doesSessionExist();
+      if (!isAuthenticated) {
+        navigate(ROOT_ROUTES.LOGIN);
+      }
+    }
+
+    void fetchSession();
+  }, [navigate]);
+
+  return <>{children}</>;
+};
 
 const router = createBrowserRouter([
   {
     path: ROOT_ROUTES.ROOT,
-    //++ TODO add auth wrapper here
-    // if session exists: render calendar
-    // if not, redirect to login
-    element: <Calendar />,
+    element: (
+      <ProtectedRoute>
+        <CalendarView />
+      </ProtectedRoute>
+    ),
   },
   { path: ROOT_ROUTES.LOGIN, element: <LoginView /> },
   { path: ROOT_ROUTES.LOGOUT, element: <LogoutView /> },
--- packages/web/src/views/Calendar/Calendar.tsx
+++ packages/web/src/views/Calendar/Calendar.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
-import Session from "supertokens-auth-react/recipe/session";
 import { FlexDirections } from "@web/components/Flex/styled";
 import { ID_MAIN } from "@web/common/constants/web.constants";
-import { AbsoluteOverflowLoader } from "@web/components/AbsoluteOverflowLoader";
-import { ROOT_ROUTES } from "@web/common/constants/routes";
 
 import { Grid } from "./components/Grid/";
 import { useScroll } from "./hooks/grid/useScroll";
@@ -21,23 +17,6 @@ import { LeftSidebar } from "./components/LeftSidebar";
 import { RightSidebar } from "./components/RightSidebar";
 import { Draft } from "./components/Event/Draft";
 import { Dedication } from "./components/Dedication";
-
-// refactor to wrapper component that locks down anything
-// and returns the child if session exists
-// and redirects if not
-export const Calendar = () => {
-  const sessionContext = Session.useSessionContext();
-
-  if (scwessionContext.loading) {
-    return <AbsoluteOverflowLoader />;
-  }
-
-  if (!Session.doesSessionExist()) {
-    return <Navigate to={ROOT_ROUTES.LOGIN} />;
-  }
-
-  return <CalendarView />;
-};
 
 export const CalendarView = () => {
   const prefs = usePreferences();
