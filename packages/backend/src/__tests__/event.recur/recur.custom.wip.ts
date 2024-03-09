00c36039e442b2a23a23020d9c77bb5b754543ba
Tyler Dane <tyler@switchback.tech>
2024-03-09 16:12:08 -0600

    :wastebasket: Chore: delete dead code in recur.custom.wip.ts

--- packages/backend/src/__tests__/event.recur/recur.custom.wip.ts
+++
@@ -1,7 +0,0 @@
-// describe("Event Recurrence: Custom", () => {
-//   it.todo("supports weekly on MWF");
-//   test.todo("supports monthly recurrence");
-//   test.todo("supports monthly on date recurrence");
-//   test.todo("supports monthly on week # recurrence");
-//   test.todo("supports annual recurrence");
-// });
