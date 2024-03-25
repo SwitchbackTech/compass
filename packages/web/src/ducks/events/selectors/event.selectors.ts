ef547fc8c7c86c70865cec79e3fc90befaee1f87
Tyler Dane <tyler@switchback.tech>
2024-03-24 21:06:08 -0500

    perf: memoize event+draft selectors (#99)

--- packages/web/src/ducks/events/selectors/event.selectors.ts
+++ packages/web/src/ducks/events/selectors/event.selectors.ts
@@ -3,92 +3,40 @@ import { Schema_Event } from "@core/types/event.types";
 import { RootState } from "@web/store";
 import { assignEventsToRow } from "@web/common/utils/grid.util";
 
-export const selectAllDayEvents = (state: RootState) => {
-  const entities = state.events.entities.value || {};
-  const weekIds = state.events.getWeekEvents.value || [];
-  if (!weekIds.data || weekIds.data.length === 0) return [];
-  const weekEventsMapped: Schema_Event[] = weekIds.data.map(
-    (_id: string) => entities[_id]
-  );
+export const selectAllDayEvents = createSelector(
+  (state: RootState) => state.events.entities.value || {},
+  (state: RootState) => state.events.getWeekEvents.value || [],
+  (entities, weekIds) => {
+    if (!("data" in weekIds) || weekIds.data.length === 0) return [];
 
-  const _allDayEvents = weekEventsMapped.filter(
-    (e: Schema_Event) => e !== undefined && e.isAllDay
-  );
-
-  const { allDayEvents } = assignEventsToRow(_allDayEvents);
-  return allDayEvents;
-};
-
-export const selectEventEntities = (state: RootState) =>
-  state.events.entities.value || {};
-
-export const selectEventById = (state: RootState, id: string): Schema_Event =>
-  selectEventEntities(state)[id] || {};
-
-export const selectGridEvents = (state: RootState): Schema_Event[] => {
-  const entities = state.events.entities.value || {};
-  const weekIds = state.events.getWeekEvents.value || [];
-  if (!weekIds.data || weekIds.data.length === 0) return [];
-  const weekEventsMapped = weekIds.data.map((_id: string) => entities[_id]);
-
-  const weekEvents = weekEventsMapped.filter(
-    (e: Schema_Event) => e !== undefined && !e.isAllDay
-  );
-
-  return weekEvents;
-};
-
-/*********************
- * Memoized Selectors
- *    ... that aren't being allowed to
- *        memoized correctly
- *        due to how many dependencies
- *        are currently in useGetWeekViewProps
- * 
- * How to use:
-    const s = store.getState();
-    const allDayEvents = selectAllDayEventsMemo(s);
-    const weekEvents = selectWeekEventsMemo(s);
- * ^ Using this way is really slow for some reason
- *********************/
-const _selectWeekEntities = (state: RootState) => {
-  if (state.events === undefined) return {};
-  return state.events.entities.value;
-};
-const _selectWeekIds = (state: RootState) => {
-  if (state.events === undefined) return [];
-  return state.events.getWeekEvents.value;
-};
-
-export const selectAllDayEventsMemo = createSelector(
-  _selectWeekIds,
-  _selectWeekEntities,
-  (weekIds, weekEntities) => {
-    if (weekIds === null) return [];
-    const weekEventsMapped = weekIds.data.map(
-      (_id: string) => weekEntities[_id]
+    const weekEvents: Schema_Event[] = weekIds.data.map(
+      (_id: string) => entities[_id]
     );
-
-    const _allDayEvents: Schema_Event[] = weekEventsMapped.filter(
+    const _allDayEvents: Schema_Event[] = weekEvents.filter(
       (e: Schema_Event) => e !== undefined && e.isAllDay
     );
-
     const { allDayEvents } = assignEventsToRow(_allDayEvents);
     return allDayEvents;
   }
 );
 
-export const selectWeekEventsMemo = createSelector(
-  _selectWeekIds,
-  _selectWeekEntities,
-  (weekIds, weekEntities) => {
-    if (weekIds === null) return [];
-    const weekEventsMapped = weekIds.data.map(
-      (_id: string) => weekEntities[_id]
-    );
+export const selectEventById = (state: RootState, id: string): Schema_Event =>
+  selectEventEntities(state)[id] || {};
+
+export const selectEventEntities = (state: RootState) =>
+  state.events.entities.value || {};
+
+export const selectGridEvents = createSelector(
+  (state: RootState) => state.events.entities.value || {},
+  (state: RootState) => state.events.getWeekEvents.value || [],
+  (entities, weekIds) => {
+    if (!("data" in weekIds) || weekIds.data.length === 0) return [];
+    const weekEventsMapped = weekIds.data.map((_id: string) => entities[_id]);
+
     const weekEvents = weekEventsMapped.filter(
       (e: Schema_Event) => e !== undefined && !e.isAllDay
     );
+
     return weekEvents;
   }
 );
