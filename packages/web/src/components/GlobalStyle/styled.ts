a2e65929ed4daa219ed971bae71604ad8e67168b
Azhar Ahmed <126657393+AzharAhmed-bot@users.noreply.github.com>
2024-05-14 17:56:29 -0400

    💄 style: set background color (#104)
    *  set background color for consistency after scroll and adding event
    
    *  update overflow on x and y axis consistency

--- packages/web/src/components/GlobalStyle/styled.ts
+++ packages/web/src/components/GlobalStyle/styled.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import { ZIndex } from "@web/common/constants/web.constants";
+import { BASE_COLORS } from "@core/constants/colors";
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -9,6 +10,8 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     margin: 0;
+    background-color:${BASE_COLORS.DEEP_BLUE};
+    overflow-x: hidden;
   }
 
   .react-datepicker-popper {
