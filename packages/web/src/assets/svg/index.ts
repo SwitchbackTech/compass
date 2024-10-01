63442ffa2c58dfb3cc6bfbd1075c0f288ae917fb
Tyler Dane <tyler@switchback.tech>
2024-10-01 07:15:23 -0500

    chore: delete custom SVG component

--- packages/web/src/assets/svg/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ArrowDownFilledIcon } from "./arrowDownFilled.svg";
-export { default as CheckIcon } from "./check.svg";
-export { default as MonthCalendarIcon } from "./monthCalendar.svg";
-export { default as TrashIcon } from "./trash.svg";
