ccb33c9634e847d4b74117d130c288e2842e3a9d
Tyler Dane <tyler@switchback.tech>
2024-10-20 07:27:58 -0500

    style: remove TodayButton border

--- packages/web/src/views/Calendar/components/TodayButton/styled.ts
+++ packages/web/src/views/Calendar/components/TodayButton/styled.ts
@@ -1,13 +1,9 @@
 import styled from "styled-components";
-import { ColorNames } from "@core/types/color.types";
-import { getColor } from "@core/util/color.utils";
 import { Flex } from "@web/components/Flex";
 
 export const StyledTodayButton = styled(Flex)`
   align-items: center;
-  border: 1px solid ${getColor(ColorNames.GREY_4)};
-  border-radius: 2.5px;
-  color: ${getColor(ColorNames.GREY_4)};
+  color: ${({ theme }) => theme.color.text.primary};
   cursor: pointer;
   display: flex;
   font-size: 17px;
