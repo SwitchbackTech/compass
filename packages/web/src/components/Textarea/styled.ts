0ed104347ec4a74d94f0c59838d2c11783b84a51
Tyler Dane <tyler@switchback.tech>
2024-10-29 07:19:14 -0500

    style: Textarea

--- packages/web/src/components/Textarea/styled.ts
+++ packages/web/src/components/Textarea/styled.ts
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 import TextareaAutoSize from "react-textarea-autosize";
-import { inputBaseStyles } from "@web/common/styles/components";
 
 import { Props } from "./types";
 
 export const Styled = styled(TextareaAutoSize)<Props>`
-  ${inputBaseStyles}
   border: none;
   outline: none;
   font-weight: 600;
   width: 100%;
   resize: none;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.color.text.darkPlaceholder};
+  }
 `;
