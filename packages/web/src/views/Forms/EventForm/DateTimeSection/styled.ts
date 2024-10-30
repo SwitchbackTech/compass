a9ee6afcddebaf4a202e330534dc75eb3ba2a386
Tyler Dane <tyler@switchback.tech>
2024-10-30 08:04:25 -0500

    style: add margin to form Description

--- packages/web/src/views/Forms/EventForm/DateTimeSection/styled.ts
+++ packages/web/src/views/Forms/EventForm/DateTimeSection/styled.ts
@@ -3,6 +3,7 @@ import { Flex } from "@web/components/Flex";
 
 export const StyledDateTimeFlex = styled(Flex)`
   margin-top: 15px;
+  margin-bottom: 10px;
 `;
 
 export const StyledDateFlex = styled(Flex)`
--- packages/web/src/views/Forms/EventForm/RepeatSection/styled.ts
+++ packages/web/src/views/Forms/EventForm/RepeatSection/styled.ts
@@ -2,7 +2,7 @@ import { Flex } from "@web/components/Flex";
 import styled from "styled-components";
 
 export const StyledRepeatContainer = styled.div`
-  padding-bottom: 10px;
+  margin-bottom: 10px;
   &:hover {
     cursor: pointer;
   }
