72b7d93cc1910d653836db8c8ebc7b44daa7d4c2
Tyler Dane <tyler@switchback.tech>
2024-11-04 07:54:26 -0600

    style: Command

--- packages/web/src/components/Icons/Command.tsx
+++ packages/web/src/components/Icons/Command.tsx
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import { Command } from "@phosphor-icons/react";
-import { ColorNames } from "@core/types/color.types";
-import { getColor } from "@core/util/color.utils";
 
 export const StyledCommandIcon = styled(Command)`
-  color: ${getColor(ColorNames.GREY_5)};
+  color: ${({ theme }) => theme.color.text.lighter};
   transition: filter 0.2s ease;
 
   &:hover {
