086987428a9be96c40f18a4430945fc21a2af50d
Tyler Dane <tyler@switchback.tech>
2024-11-05 08:04:17 -0600

    chore: remove getNeighbourKey util

--- packages/core/src/util/color.utils.ts
+++ packages/core/src/util/color.utils.ts
@@ -7,22 +7,3 @@ export const brighten = (color: string, amount?: number) => {
 export const darken = (color: string, amount?: number) => {
   return tinycolor(color).darken(amount).toString();
 };
-
-export const getNeighbourKey = (key = "", obj = {}, diff = 1) => {
-  const splitKeys = key.split("_");
-  if (splitKeys === undefined || splitKeys.length < 1) {
-    throw new Error(
-      'You should provide object with keys "ANYTHING_KEY_NUMBER" where NUMBER is number'
-    );
-  }
-
-  const prev = splitKeys.length - 1;
-  //@ts-ignore
-  const index = +splitKeys[prev];
-  const propName = splitKeys.filter((value) => value !== `${index}`).join("_");
-
-  const neighbourIndex = index + diff;
-  const neighbourKey = `${propName}_${neighbourIndex}`;
-
-  return Object.keys(obj).find((_key) => _key === neighbourKey) || key;
-};
