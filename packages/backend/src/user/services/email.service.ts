3e9ab093f7a9d9c86acb03c1c63930d922351449
Muhammed Aldulaimi <muhammed.aldulaimi98@gmail.com>
2024-12-26 06:47:21 +0300

    fix: Optional email service proper handling

--- packages/backend/src/user/services/email.service.ts
+++ packages/backend/src/user/services/email.service.ts
@@ -8,16 +8,41 @@ const logger = Logger("app:emailer.service");
 
 class EmailService {
   addToEmailList = async (email: string, firstName: string) => {
-    const url = `https://api.convertkit.com/v3/tags/${ENV.EMAILER_LIST_ID}/subscribe?api_secret=${ENV.EMAILER_SECRET}&email=${email}&first_name=${firstName}`;
-
-    const response = await axios.post(url);
-
-    if (response.status !== 200) {
-      throw error(EmailerError.AddToListFailed, "Failed to add email to list");
-      logger.error(response.data);
+    if (!ENV.EMAILER_LIST_ID && !ENV.EMAILER_SECRET) {
+      logger.warn(
+        "Email service is disabled. Required environment variables are missing."
+      );
+      return;
     }
 
-    return response;
+    const url = `https://api.convertkit.com/v3/tags/${ENV.EMAILER_LIST_ID}/subscribe?api_secret=${ENV.EMAILER_SECRET}&email=${email}&first_name=${firstName}`;
+
+    try {
+      const response = await axios.post(url);
+
+      if (response.status !== 200) {
+        throw error(
+          EmailerError.AddToListFailed,
+          "Failed to add email to list"
+        );
+        logger.error(response.data);
+      }
+
+      return response;
+    } catch (e) {
+      if (
+        axios.isAxiosError(e) &&
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        e?.response?.data?.message === "API Key not valid"
+      ) {
+        throw error(
+          EmailerError.IncorrectApiKey,
+          "Incorrect API key. Please make sure environment variables beginning with EMAILER_ are set correctly or set to an empty string."
+        );
+      }
+
+      throw e;
+    }
   };
 }
 
