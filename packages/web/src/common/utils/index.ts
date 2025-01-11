da14eb0d3527ced1d8e91d38a1fb155f974a5e6d
Muhammed Aldulaimi <muhammed.aldulaimi98@gmail.com>
2025-01-11 08:43:39 +0300

    bug(web): Enhance UX for draft event start date time calculation on mousedown
    This commit makes the mousedown more accurate to the click by rounding down instead of rounding up to the nearest minute skip

--- packages/web/src/common/utils/index.ts
+++ packages/web/src/common/utils/index.ts
@@ -34,6 +34,9 @@ export const roundToNearest = (x: number, roundBy: number) =>
 export const roundToNext = (number: number, roundBy: number): number =>
   Math.ceil(number / roundBy) * roundBy;
 
+export const roundToPrev = (number: number, roundBy: number): number =>
+  Math.floor(number / roundBy) * roundBy;
+
 export const sortLowToHigh = (vals: number[]) =>
   [...vals].sort(function (a, b) {
     return a - b;
