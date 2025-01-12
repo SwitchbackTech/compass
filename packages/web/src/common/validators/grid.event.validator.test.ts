a0a2fb40fc487d5c9cf3ea606a62085af08a6c1c
Tyler Dane <tyler@switchback.tech>
2025-01-12 08:07:14 -0600

    feat: validate events against schema using zod
    this allows us to be confident in the event type, striping unexpected properties along the way

---
+++ packages/web/src/common/validators/grid.event.validator.test.ts
@@ -0,0 +1,48 @@
+import { Origin, Priorities } from "@core/constants/core.constants";
+
+import { Schema_GridEvent } from "../types/web.event.types";
+import { validateGridEvent } from "./grid.event.validator";
+
+describe("validateGridEvent", () => {
+  it("strips unexpected properties", () => {
+    const event = {
+      startDate: "2023-01-01",
+      endDate: "2023-01-02",
+      origin: Origin.COMPASS,
+      priority: Priorities.RELATIONS,
+      user: "user123",
+      unexpectedProperty: "unexpectedValue",
+    };
+
+    const parsedEvent = validateGridEvent(event);
+    expect(parsedEvent).not.toHaveProperty("unexpectedProperty");
+  });
+  it("validates a correct event", () => {
+    const event: Schema_GridEvent = {
+      endDate: "2023-01-02",
+      hasFlipped: true,
+      isOpen: true,
+      row: 10,
+      startDate: "2023-01-01",
+      origin: Origin.COMPASS,
+      priority: Priorities.RELATIONS,
+      user: "user123",
+    };
+
+    const parsedEvent = validateGridEvent(event);
+    expect(parsedEvent).toEqual(event);
+  });
+
+  it("invalidates when types are incorrect", () => {
+    const event = {
+      startDate: "2022-10-22",
+      endDate: "2023-01-02",
+      origin: Origin.UNSURE,
+      priority: Priorities.SELF,
+      user: "user1",
+      hasFlipped: "true", // invalid
+    } as unknown as Schema_GridEvent;
+
+    expect(() => validateGridEvent(event)).toThrow();
+  });
+});
---
+++ packages/web/src/common/validators/grid.event.validator.ts
@@ -0,0 +1,8 @@
+import { Schema_Event } from "@core/types/event.types";
+
+import { GridEventSchema } from "../types/web.event.types";
+
+export const validateGridEvent = (event: Schema_Event) => {
+  const result = GridEventSchema.parse(event);
+  return result;
+};
