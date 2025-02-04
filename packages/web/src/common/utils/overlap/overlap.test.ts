3079fac82dd68ef2532ce106bf1a8a7fc4a70744
Tyler Dane <tyler@switchback.tech>
2025-02-04 09:47:23 -0600

    🧪 Feat: move overlap to dedicated dir and add tests (#246)
    * 🧪 Feat: move overlap to dedicated dir and add tests
    
    * ♻️ Refactor: Remove deprecated overlap tests and streamline event adjustment logic

---
+++ packages/web/src/common/utils/overlap/overlap.test.ts
@@ -0,0 +1,150 @@
+import { Categories_Event } from "@core/types/event.types";
+import { getUserId } from "@web/auth/auth.util";
+import { Schema_GridEvent } from "@web/common/types/web.event.types";
+
+import { assembleDefaultEvent } from "../event.util";
+import { adjustOverlappingEvents } from "./overlap";
+
+jest.mock("@web/auth/auth.util", () => ({
+  getUserId: jest.fn(),
+}));
+
+export const assembleTimedGridEvent = async (
+  startTime: string,
+  endTime: string,
+  id?: string
+) => {
+  const _time = (time: string) => `2025-01-27T${time}:00+03:00`;
+
+  const _event = await assembleDefaultEvent(
+    Categories_Event.TIMED,
+    _time(startTime),
+    _time(endTime)
+  );
+
+  const event = id ? { ..._event, _id: id } : _event;
+  return event as Schema_GridEvent;
+};
+
+describe("adjustOverlappingEvents", () => {
+  beforeEach(() => {
+    (getUserId as jest.Mock).mockResolvedValue("mockUser");
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Sorts events by start time", async () => {
+    const eventA = await assembleTimedGridEvent("07:30", "08:00");
+    const eventB = await assembleTimedGridEvent("07:15", "07:45");
+
+    const adjustedEvents = adjustOverlappingEvents([eventA, eventB]);
+
+    expect(adjustedEvents[0].startDate).toBe(eventB.startDate);
+    expect(adjustedEvents[1].startDate).toBe(eventA.startDate);
+  });
+
+  it("Returns the same array if no events overlap", async () => {
+    const eventA = await assembleTimedGridEvent("07:30", "08:00");
+    const eventB = await assembleTimedGridEvent("08:00", "08:30");
+    const adjustedEvents = adjustOverlappingEvents([eventA, eventB]);
+
+    expect(adjustedEvents).toEqual([eventA, eventB]);
+  });
+
+  it("Adjusts events that overlap", async () => {
+    const eventA = await assembleTimedGridEvent("07:30", "08:00");
+    const eventB = await assembleTimedGridEvent("07:45", "08:15");
+
+    const adjustedEvents = adjustOverlappingEvents([eventA, eventB]);
+
+    expect(adjustedEvents[0].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[0].position.horizontalOrder).toBe(1);
+    expect(adjustedEvents[1].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[1].position.horizontalOrder).toBe(2);
+  });
+
+  it("Does not modify events that do not overlap", async () => {
+    const eventA = await assembleTimedGridEvent("07:30", "08:00");
+    const eventB = await assembleTimedGridEvent("07:45", "08:15");
+    const nonOverlappingEventC = await assembleTimedGridEvent("09:45", "10:15");
+
+    const adjustedEvents = adjustOverlappingEvents([
+      eventA,
+      eventB,
+      nonOverlappingEventC,
+    ]);
+
+    expect(adjustedEvents[0].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[0].position.horizontalOrder).toBe(1);
+    expect(adjustedEvents[1].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[1].position.horizontalOrder).toBe(2);
+    // Non-overlapping event should not be modified
+    expect(adjustedEvents[2]).toStrictEqual(nonOverlappingEventC);
+  });
+
+  it("Adjusts events that indirectly overlap", async () => {
+    const eventA = await assembleTimedGridEvent("07:30", "08:00");
+    const eventB = await assembleTimedGridEvent("07:45", "08:15");
+    // `eventC` starts when `eventA` ends, does not directly overlap,
+    // but should still be adjusted because it indirectly overlaps due to `eventB`
+    const eventC = await assembleTimedGridEvent("08:00", "08:30");
+
+    const adjustedEvents = adjustOverlappingEvents([eventA, eventB, eventC]);
+
+    expect(adjustedEvents[0].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[0].position.horizontalOrder).toBe(1);
+    expect(adjustedEvents[1].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[1].position.horizontalOrder).toBe(2);
+    expect(adjustedEvents[2].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[2].position.horizontalOrder).toBe(3);
+  });
+
+  it("Adjusts events that overlap in 2 distinct groups", async () => {
+    // // Group 1
+    const eventA = await assembleTimedGridEvent("07:30", "08:00", "eventA");
+    const eventB = await assembleTimedGridEvent("07:45", "08:15", "eventB");
+    const eventC = await assembleTimedGridEvent("08:00", "08:30", "eventC");
+
+    // Group 2
+    const eventD = await assembleTimedGridEvent("10:00", "11:00", "eventD");
+    const eventE = await assembleTimedGridEvent("10:30", "11:30", "eventE");
+
+    const adjustedEvents = adjustOverlappingEvents([
+      eventA,
+      eventB,
+      eventC,
+      eventD,
+      eventE,
+    ]);
+
+    // Group 1
+    expect(adjustedEvents[0].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[0].position.horizontalOrder).toBe(1);
+    expect(adjustedEvents[1].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[1].position.horizontalOrder).toBe(2);
+    expect(adjustedEvents[2].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[2].position.horizontalOrder).toBe(3);
+
+    // Group 2
+    expect(adjustedEvents[3].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[3].position.horizontalOrder).toBe(1);
+    expect(adjustedEvents[4].position.isOverlapping).toBe(true);
+    expect(adjustedEvents[4].position.horizontalOrder).toBe(2);
+  });
+
+  it("Orders alphabetically if start and end times are the same", async () => {
+    const _eventA = await assembleTimedGridEvent("07:30", "08:00");
+    const eventA = { ..._eventA, title: "A" };
+
+    const _eventB = await assembleTimedGridEvent("07:30", "08:00");
+    const eventB = { ..._eventB, title: "B" };
+
+    const adjustedEvents = adjustOverlappingEvents([eventA, eventB]);
+
+    expect(adjustedEvents[0].title).toBe("A");
+    expect(adjustedEvents[0].position.horizontalOrder).toBe(1);
+    expect(adjustedEvents[1].title).toBe("B");
+    expect(adjustedEvents[1].position.horizontalOrder).toBe(2);
+  });
+});
---
+++ packages/web/src/common/utils/overlap/overlap.ts
@@ -0,0 +1,86 @@
+import dayjs from "dayjs";
+import { Schema_GridEvent } from "@web/common/types/web.event.types";
+
+export const adjustOverlappingEvents = (
+  events: Schema_GridEvent[]
+): Schema_GridEvent[] => {
+  let adjustedEvents = deepCopyEvents(events);
+  adjustedEvents.sort((a, b) => dayjs(a.startDate).diff(dayjs(b.startDate)));
+
+  const processedEvents = new Set<string>();
+
+  for (const event of adjustedEvents) {
+    if (processedEvents.has(event._id)) continue;
+
+    const overlappingEventsSet = findOverlaps(event, adjustedEvents);
+    const eventGroup = Array.from(overlappingEventsSet);
+
+    if (eventGroup.length > 1) {
+      adjustEventGroup(eventGroup);
+      eventGroup.forEach((e) => processedEvents.add(e._id));
+    }
+  }
+  return adjustedEvents;
+};
+
+const findOverlaps = (
+  event: Schema_GridEvent,
+  adjustedEvents: Schema_GridEvent[],
+  accumulatedEvents = new Set<Schema_GridEvent>()
+): Set<Schema_GridEvent> => {
+  const directOverlaps = adjustedEvents.filter(
+    (otherEvent) =>
+      otherEvent !== event &&
+      !accumulatedEvents.has(otherEvent) &&
+      dayjs(event.startDate).isBefore(dayjs(otherEvent.endDate)) &&
+      dayjs(event.endDate).isAfter(dayjs(otherEvent.startDate))
+  );
+
+  directOverlaps.forEach((overlappingEvent) => {
+    accumulatedEvents.add(overlappingEvent);
+    findOverlaps(overlappingEvent, adjustedEvents, accumulatedEvents);
+  });
+
+  return accumulatedEvents;
+};
+
+const adjustEventGroup = (eventGroup: Schema_GridEvent[]) => {
+  eventGroup.sort((a, b) => dayjs(a.startDate).diff(dayjs(b.startDate)));
+
+  if (eventsHaveExactSameTimes(eventGroup)) {
+    sortEventsByTitle(eventGroup);
+  }
+
+  const multiplier = roundToTwoDecimals(1 / eventGroup.length);
+
+  eventGroup.forEach((event, index) => {
+    event.position.isOverlapping = true;
+    event.position.widthMultiplier *= multiplier;
+    event.position.horizontalOrder = index + 1;
+  });
+};
+
+const roundToTwoDecimals = (value: number): number => {
+  return Math.round(value * 100) / 100;
+};
+
+const eventsHaveExactSameTimes = (eventGroup: Schema_GridEvent[]): boolean => {
+  return eventGroup.every(
+    (event) =>
+      dayjs(event.startDate).isSame(eventGroup[0].startDate) &&
+      dayjs(event.endDate).isSame(eventGroup[0].endDate)
+  );
+};
+
+const sortEventsByTitle = (eventGroup: Schema_GridEvent[]) => {
+  eventGroup.sort((a, b) =>
+    a.title && b.title ? a.title.localeCompare(b.title) : 0
+  );
+};
+
+const deepCopyEvents = (events: Schema_GridEvent[]): Schema_GridEvent[] => {
+  return events.map((event) => ({
+    ...event,
+    position: { ...event.position },
+  }));
+};
