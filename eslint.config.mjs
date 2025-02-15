cf76e38d12b7a4467fc305a41f528c9ec3b00957
Tyler Dane <tyler@switchback.tech>
2025-02-15 12:44:18 -0600

    feat(checkpoint): update eslint config - named imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,24 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+import { pluginJs as jsPlugin } from "@eslint/js";
+import { tseslint as tsEslintPlugin } from "typescript-eslint";
+import { pluginReact as reactPlugin } from "eslint-plugin-react";
+import { reactHooks as reactHooksPlugin } from "eslint-plugin-react-hooks";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  jsPlugin.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
+  ...tsEslintPlugin.configs.recommended,
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat["jsx-runtime"],
-  ...tseslint.configs.recommended,
   {
     settings: {
       react: {
         version: "detect",
       },
     },
-    plugins: { "react-hooks": reactHooks },
+    plugins: { "react-hooks": reactHooksPlugin },
     rules: {
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
