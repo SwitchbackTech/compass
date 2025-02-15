74187f23c268aef030bd8ff08bf7ce1a1f70b82e
Tyler Dane <tyler@switchback.tech>
2025-02-15 14:02:12 -0600

    feat(checkpoint): update eslint config - reactHooks plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,16 @@
 import globals from "globals";
-import { pluginJs as jsPlugin } from "@eslint/js";
-import { tseslint as tsEslintPlugin } from "typescript-eslint";
-import { pluginReact as reactPlugin } from "eslint-plugin-react";
-import { reactHooks as reactHooksPlugin } from "eslint-plugin-react-hooks";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import pluginJest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  jsPlugin.configs.recommended,
-  reactPlugin.configs.flat.recommended,
-  reactPlugin.configs.flat["jsx-runtime"],
-  ...tsEslintPlugin.configs.recommended,
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  ...tseslint.configs.recommended,
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   {
@@ -18,10 +19,25 @@ export default [
         version: "detect",
       },
     },
-    plugins: { "react-hooks": reactHooksPlugin },
+    plugins: { "react-hooks": reactHooks },
     rules: {
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    // Rules and plugins for tests only
+    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
     },
   },
 ];
