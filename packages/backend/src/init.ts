a6eb201dd24bc2de5c87092482d4a19ff14a7236
Tyler Dane <tyler@switchback.tech>
2025-02-22 10:19:40 -0600

    🔧 Chore: Fix runtime error by defining @core alias before instantiating logger

--- packages/backend/src/init.ts
+++ packages/backend/src/init.ts
@@ -1,13 +1,13 @@
+// sort-imports-ignore
 import dotenv from "dotenv";
 import moduleAlias from "module-alias";
 import path from "path";
-// eslint-disable-next-line prettier/prettier
-import { Logger } from "@core/logger/winston.logger";
-
 moduleAlias.addAliases({
   "@backend": `${__dirname}`,
   "@core": `${path.resolve(__dirname, "../../core/src")}`,
 });
+// eslint-disable-next-line prettier/prettier
+import { Logger } from "@core/logger/winston.logger";
 
 const dotenvResult = dotenv.config();
 if (dotenvResult.error) {
