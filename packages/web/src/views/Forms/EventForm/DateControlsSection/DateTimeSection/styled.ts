b7a6afd070435aaa0ade4b9f01b66be84b18f90a
Muhammed Aldulaimi <muhammed.aldulaimi98@gmail.com>
2025-03-24 21:53:41 +0300

    chore(web): move margins styling to parent `DateControlsSection` component
    Since we will add `RecurrenceSection`, by moving these margins related styles to the date controls component, we make the margins consistent inside date controls and we repeat our code less.

--- packages/web/src/views/Forms/EventForm/DateControlsSection/DateTimeSection/styled.ts
+++ packages/web/src/views/Forms/EventForm/DateControlsSection/DateTimeSection/styled.ts
@@ -1,11 +1,6 @@
 import styled from "styled-components";
 import { Flex } from "@web/components/Flex";
 
-export const StyledDateTimeFlex = styled(Flex)`
-  margin-top: 15px;
-  margin-bottom: 10px;
-`;
+export const StyledDateTimeFlex = styled(Flex)``;
 
-export const StyledTimeFlex = styled(Flex)`
-  margin-left: 40px;
-`;
+export const StyledTimeFlex = styled(Flex)``;
--- packages/web/src/views/Forms/EventForm/DateControlsSection/styled.ts
+++ packages/web/src/views/Forms/EventForm/DateControlsSection/styled.ts
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const StyledControlsSection = styled.div``;
+export const StyledControlsSection = styled.div`
+  margin-top: 15px;
+  margin-bottom: 10px;
+  margin-left: 40px;
+`;
