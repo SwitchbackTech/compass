88d0f0f622e97c89034d068964ac852c97b53769
Tyler Dane <tyler@switchback.tech>
2025-04-08 10:00:01 -0500

    ✨ Feat: add support for handling recurrence cancellations

--- packages/backend/src/sync/services/notify/handler/gcal.notification.handler.ts
+++ packages/backend/src/sync/services/notify/handler/gcal.notification.handler.ts
@@ -3,6 +3,7 @@ import { gCalendar } from "@core/types/gcal";
 import { SyncError } from "@backend/common/constants/error.constants";
 import { error } from "@backend/common/errors/handlers/error.handler";
 import gcalService from "@backend/common/services/gcal/gcal.service";
+import { RecurringEventRepository } from "@backend/event/queries/event.recur.queries";
 import { getSync, updateSyncTokenFor } from "@backend/sync/util/sync.queries";
 import { Summary_Sync } from "../../../sync.types";
 import { GcalSyncProcessor } from "../../sync/gcal.sync.processor";
@@ -13,6 +14,7 @@ export class GCalNotificationHandler {
   constructor(
     private gcal: gCalendar,
     private userId: string,
+    private repo: RecurringEventRepository,
   ) {}
 
   /**
@@ -30,7 +32,7 @@ export class GCalNotificationHandler {
 
     if (hasChanges) {
       console.log("\nCHANGES TO PROCESS:", changes.length);
-      const processor = new GcalSyncProcessor(this.gcal, this.userId);
+      const processor = new GcalSyncProcessor(this.repo);
       const changeSummary = await processor.processEvents(changes);
       console.log("\nRESULT:", changeSummary);
       return { summary: "PROCESSED", changes: changeSummary };
--- packages/backend/src/sync/services/sync/gcal.sync.processor.ts
+++ packages/backend/src/sync/services/sync/gcal.sync.processor.ts
@@ -1,19 +1,15 @@
-import { gCalendar, gSchema$Event } from "@core/types/gcal";
+import { gSchema$Event } from "@core/types/gcal";
+import { EventError } from "@backend/common/constants/error.constants";
+import { error } from "@backend/common/errors/handlers/error.handler";
+import { findCompassEventBy } from "@backend/event/queries/event.queries";
+import { RecurringEventRepository } from "@backend/event/queries/event.recur.queries";
 import { GcalParser } from "@backend/event/services/recur/util/recur.gcal.util";
 import { Change_Gcal, Operation_Sync } from "../../sync.types";
 
 export class GcalSyncProcessor {
-  constructor(
-    private gcal: gCalendar,
-    private userId: string,
-  ) {}
+  constructor(private repo: RecurringEventRepository) {}
 
   async processEvents(events: gSchema$Event[]): Promise<Change_Gcal[]> {
-    console.log(
-      "gcal",
-      this.gcal?.settings?.context?._options?.auth?.toString(),
-      this.userId,
-    );
     const summary: Change_Gcal[] = [];
     console.log(`Processing ${events.length} events...`);
     for (const event of events) {
@@ -22,23 +18,40 @@ export class GcalSyncProcessor {
       const status = parser.status;
       const change = parser.summarize();
       let operation: Operation_Sync = null;
-      // --- Cancellation Logic ---
+
+      if (!event.id) {
+        throw error(
+          EventError.MissingGevents,
+          "Event not processed due to missing id",
+        );
+      }
+
+      // --- Handle cancellation ---
       if (status === "CANCELLED") {
         if (category === "RECURRENCE_BASE") {
-          // Cancelled Master Event
-          // console.log(`Processing cancelled base: ${event.summary}`);
+          // find the base event by looking for the
+          // compass event with the matching gEventId
+          const result = await findCompassEventBy("gEventId", event.id);
+          if (!result.eventExists) {
+            throw error(
+              EventError.MissingGevents,
+              "Did not cancel series becauase base event not found",
+            );
+          }
+          console.log(
+            `Matched these ids: ${result.event._id}(Compass) and ${event.id} (Gcal)`,
+          );
+          await this.repo.cancelSeries(result.event._id as string);
           operation = "CANCELLED";
-          // TODO: delete base + linked instances
         } else {
-          // console.log(`Processing cancelled instance: ${event.summary}`);
+          await this.repo.cancelInstance(event.id, { idKey: "gEventId" });
           operation = "CANCELLED";
         }
       }
 
-      // --- Upsert Logic for Active Events ---
+      // --- Handle upsert for active event ---
       else {
         /*
-        START HERE ON TUESDAY
         - init a mapper
         - map to compass schema (base or instance)
         - upsert (see ref file)
