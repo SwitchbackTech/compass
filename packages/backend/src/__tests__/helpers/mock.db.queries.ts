8be0f7aab6dce45b2a547657acce75345d95442c
Tyler Dane <tyler@switchback.tech>
2025-04-13 06:19:44 -0500

    🧪 Test: add getCategorizedEventsInDb test utility

--- packages/backend/src/__tests__/helpers/mock.db.queries.ts
+++ packages/backend/src/__tests__/helpers/mock.db.queries.ts
@@ -1,5 +1,16 @@
+import { Event_Core } from "@core/types/event.types";
 import { Collections } from "@backend/common/constants/collections";
 import mongoService from "@backend/common/services/mongo.service";
+
+export const getCategorizedEventsInDb = async () => {
+  const allEvents = (await getEventsInDb()) as unknown as Event_Core[];
+  const baseEvents = allEvents.filter((e) => e.recurrence?.rule !== undefined);
+  const instanceEvents = allEvents.filter(
+    (e) => e.recurrence?.eventId !== undefined,
+  );
+  const regularEvents = allEvents.filter((e) => e.recurrence === undefined);
+  return { baseEvents, instanceEvents, regularEvents };
+};
 
 export const getEventsInDb = async () => {
   return await mongoService.db.collection(Collections.EVENT).find().toArray();
