ee10f6a81b6959b0bf09f6f46bf41df405d1b188
Tyler Dane <tyler@switchback.tech>
2025-04-15 09:25:45 -0500

    🧪 Test: update simulateDbAfterGcalImport to point compass instances to their base

--- packages/backend/src/__tests__/helpers/mock.events.init.ts
+++ packages/backend/src/__tests__/helpers/mock.events.init.ts
@@ -1,17 +1,19 @@
-import { Db, WithoutId } from "mongodb";
+import { Db, ObjectId, WithoutId } from "mongodb";
 import { Origin } from "@core/constants/core.constants";
 import { MapEvent } from "@core/mappers/map.event";
 import { Schema_Event } from "@core/types/event.types";
 import { Collections } from "@backend/common/constants/collections";
+import { isBase } from "@backend/event/services/recur/util/recur.util";
 import { mockGcalEvents } from "../mocks.gcal/mocks.gcal/factories/gcal.event.factory";
 
 /**
  * Simulates the events in the database after gcal import
  * @param {Db} db - The database
+ * @param {string} userId - The user id
  * @returns {Object} - The gcal and compass events
  */
-export const simulateDbAfterGcalImport = async (db: Db) => {
-  const { gcalEvents, compassEvents } = mockGcalAndCompassEvents();
+export const simulateDbAfterGcalImport = async (db: Db, userId?: string) => {
+  const { gcalEvents, compassEvents } = mockGcalAndCompassEvents(userId);
   await db
     .collection(Collections.EVENT)
     .insertMany(compassEvents as unknown as WithoutId<Schema_Event>[]);
@@ -27,12 +29,31 @@ export const simulateDbAfterGcalImport = async (db: Db) => {
  * Generates mock compass events from gcal events
  * @returns {Object} - The gcal and compass events
  */
-export const mockGcalAndCompassEvents = () => {
+export const mockGcalAndCompassEvents = (userId?: string) => {
   const { gcalEvents } = mockGcalEvents();
   const compassEvents = MapEvent.toCompass(
-    "some-user-id",
+    userId || "some-user-id",
     gcalEvents.all,
     Origin.GOOGLE_IMPORT,
   );
-  return { gcalEvents, compassEvents };
+  const compassBase = compassEvents.find((e) => isBase(e));
+  if (!compassBase) {
+    throw new Error("No base event found");
+  }
+  const baseId = new ObjectId();
+  compassBase._id = baseId;
+  const compassEventsWithPointersToBase = compassEvents.map((e) => {
+    const isInstance = e.gRecurringEventId !== undefined;
+    if (isInstance) {
+      return {
+        ...e,
+        recurrence: {
+          ...e.recurrence,
+          eventId: baseId.toString(),
+        },
+      };
+    }
+    return e;
+  });
+  return { gcalEvents, compassEvents: compassEventsWithPointersToBase };
 };
