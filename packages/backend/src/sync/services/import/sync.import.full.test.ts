3089fccb6c3f3ff33b75227cc6de82bba8f17847
Tyler Dane <tyler@switchback.tech>
2025-04-19 14:24:01 -0500

    🐛 Fix: import first instance
    Now that we're filtering out the base from the GET call on the API, we can safely import the first instance and render it

--- packages/backend/src/sync/services/import/sync.import.full.test.ts
+++ packages/backend/src/sync/services/import/sync.import.full.test.ts
@@ -34,8 +34,9 @@ describe("SyncImport: Full", () => {
   });
 
   describe("Full import", () => {
-    it("should not import the first instance of a recurring event (just the base)", async () => {
-      // including the first instance would result in 2 events with the same start date
+    it("should import the first instance of a recurring event (and the base)", async () => {
+      // Importing both the bae and first instance helps us find the series recurrence rule.
+      // To prevent duplicates in the UI, the GET API will not return the base event
       await syncImport.importAllEvents(setup.userId, "test-calendar");
 
       const currentEventsInDb = await mongoService.event.find().toArray();
@@ -46,17 +47,19 @@ describe("SyncImport: Full", () => {
 
       const baseStart = recurringEvents[0]?.startDate;
       const firstInstanceStart = recurringEvents[1]?.startDate;
-      expect(baseStart).not.toEqual(firstInstanceStart);
+      expect(baseStart).toEqual(firstInstanceStart);
     });
 
     it("should connect instances to their base events", async () => {
       await syncImport.importAllEvents(setup.userId, "test-calendar");
       const { baseEvents, instanceEvents } = await getCategorizedEventsInDb();
 
-      expect(instanceEvents).toHaveLength(1); // 2 instances total, but 1 is skipped because it has the same start date as the base event
-      expect(instanceEvents[0]?.recurrence?.eventId).toBe(
-        baseEvents[0]?._id?.toString(),
-      );
+      expect(instanceEvents).toHaveLength(2);
+      instanceEvents.forEach((instance) => {
+        expect(instance.recurrence?.eventId).toBe(
+          baseEvents[0]?._id?.toString(),
+        );
+      });
     });
     it("should include regular and recurring events and skip cancelled events", async () => {
       const { totalProcessed, totalChanged, nextSyncToken } =
@@ -65,8 +68,8 @@ describe("SyncImport: Full", () => {
       const currentEventsInDb = await mongoService.event.find().toArray();
 
       expect(totalProcessed).toBe(5); // base + 2 instances + regular + cancelled
-      expect(totalChanged).toBe(3); // base + 1 instances + regular - cancelled
-      expect(currentEventsInDb).toHaveLength(3); // base + 1 instance + regular - cancelled
+      expect(totalChanged).toBe(4); // base + 2 instances + regular - cancelled
+      expect(currentEventsInDb).toHaveLength(4); // base + 2 instances + regular - cancelled
       // Verify we have the base recurring event
       const baseEvents = currentEventsInDb.filter(
         (e) => e.recurrence?.rule !== undefined,
@@ -78,7 +81,7 @@ describe("SyncImport: Full", () => {
       const instanceEvents = currentEventsInDb.filter(
         (e) => e.recurrence?.eventId !== undefined,
       );
-      expect(instanceEvents).toHaveLength(1); // 2 instances total, but 1 is skipped because it has the same start date as the base event
+      expect(instanceEvents).toHaveLength(2);
       expect(instanceEvents.map((e) => e.gEventId)).toEqual(
         expect.arrayContaining(["recurring-1-instance-2"]),
       );
