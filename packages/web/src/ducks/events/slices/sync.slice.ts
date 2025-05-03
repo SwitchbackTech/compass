10be3b60b1d7d067fad76f07b600a76d2af795ac
Muhammed Aldulaimi <muhammed.aldulaimi98@gmail.com>
2025-05-03 13:37:26 +0300

    ✨ feat: Enhance sync slice with reason handling
    - In 1c5873a9659a129e78f2c1a5bd868242b8f43070, we introduced context support, but we need to re-implement boilerplate that resembles that commit here again because `syncSlice` does not inherit from `createAsyncSlice`

--- packages/web/src/ducks/events/slices/sync.slice.ts
+++ packages/web/src/ducks/events/slices/sync.slice.ts
@@ -1,22 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { Sync_AsyncStateContextReason } from "@web/ducks/events/context/sync.context";
+
+type Payload_TriggerFetch = {
+  reason?: Sync_AsyncStateContextReason;
+};
 
 interface State_Sync {
   isFetchNeeded: boolean;
+  reason: null | Sync_AsyncStateContextReason;
 }
 
 const initialState: State_Sync = {
   isFetchNeeded: false,
+  reason: null,
 };
 
 export const syncSlice = createSlice({
   name: "sync",
   initialState,
   reducers: {
-    resetIsFetchNeeded: (state) => {
-      state.isFetchNeeded = false;
+    triggerFetch: {
+      reducer: (
+        state,
+        action: PayloadAction<Payload_TriggerFetch | undefined>,
+      ) => {
+        state.isFetchNeeded = true;
+        state.reason = action.payload?.reason || null;
+      },
+      prepare: (payload?: Payload_TriggerFetch) => {
+        return {
+          payload: payload || ({} as Payload_TriggerFetch),
+        };
+      },
     },
-    triggerFetch: (state) => {
-      state.isFetchNeeded = true;
+    resetIsFetchNeeded: {
+      reducer: (state) => {
+        state.isFetchNeeded = false;
+        state.reason = null;
+      },
+      prepare: () => {
+        return {
+          payload: {},
+        };
+      },
     },
   },
 });
