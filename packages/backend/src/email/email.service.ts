8f30f2f24af368df94b0f5f11adec4cc7b351408
Tyler Dane <tyler@switchback.tech>
2025-05-13 06:57:47 -0500

    ♻️ Refactor: reuse headers in email service

--- packages/backend/src/email/email.service.ts
+++ packages/backend/src/email/email.service.ts
@@ -1,55 +1,55 @@
 import axios from "axios";
+import { Subscriber, SubscriberSchema } from "@core/types/email/email.types";
+import { ENV } from "@backend/common/constants/env.constants";
 import { EmailerError } from "@backend/common/constants/error.constants";
 import { error } from "@backend/common/errors/handlers/error.handler";
 import {
-  RequestBody_CreateSubscriber,
-  RequestBody_CreateSubscriberSchema,
   Response_TagSubscriber,
   Response_UpsertSubscriber,
 } from "./email.types";
 
 class EmailService {
-  private readonly apiKey: string;
-  private readonly listId: string;
+  private static headers: { headers: Record<string, string> };
+  private static readonly baseUrl = "https://api.kit.com/v4";
 
-  constructor(apiKey: string, listId: string) {
-    this.apiKey = apiKey;
-    this.listId = listId;
+  private static initialize() {
+    if (!EmailService.headers) {
+      if (!ENV.EMAILER_SECRET) {
+        throw error(EmailerError.MissingSecret, "Did not instantiate Emailer");
+      }
+      EmailService.headers = {
+        headers: {
+          "X-Kit-Api-Key": ENV.EMAILER_SECRET,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      };
+    }
   }
 
-  async addTagToSubscriber(
-    subscriber: RequestBody_CreateSubscriber,
-    tag: string,
+  static async addTagToSubscriber(
+    subscriber: Subscriber,
+    tagId: string,
   ): Promise<Response_TagSubscriber> {
-    console.log("adding:", subscriber);
-    const upsertResult = await this.upsertSubscriber(subscriber);
-    console.log("upserted:", upsertResult);
+    EmailService.initialize();
+    console.log("adding subscriber:", subscriber.email_address);
+    // upsert subscriber
+    const upsertedSubscriber = await this.upsertSubscriber(subscriber);
+    console.log(upsertedSubscriber.subscriber.id);
+    const subId = upsertedSubscriber.subscriber.id;
 
-    const url = `https://api.kit.com/v4/tags/${
-      this.listId
-    }/subscribe?api_secret=${
-      this.apiKey
-    }&email=${subscriber.email_address}&first_name=${subscriber.first_name}`;
-
-    const data = {
-      email: subscriber.email_address,
-      tags: [tag],
-      list_id: this.listId,
-    };
-    const result = await axios.post(url, data, {
-      headers: {
-        Authorization: `Bearer ${this.apiKey}`,
-        "Content-Type": "application/json",
-      },
-    });
+    // add tag to subscriber
+    const url = `${this.baseUrl}/tags/${tagId}/subscribers/${subId}`;
+    const result = await axios.post(url, {}, this.headers);
+    console.log("result.data:", result.data);
     return result.data;
   }
 
-  async upsertSubscriber(
-    subscriber: RequestBody_CreateSubscriber,
+  static async upsertSubscriber(
+    subscriber: Subscriber,
   ): Promise<Response_UpsertSubscriber> {
-    const { data, success } =
-      RequestBody_CreateSubscriberSchema.safeParse(subscriber);
+    EmailService.initialize();
+    const { data, success } = SubscriberSchema.safeParse(subscriber);
     if (!success) {
       throw error(
         EmailerError.InvalidSubscriberData,
@@ -57,14 +57,8 @@ class EmailService {
       );
     }
 
-    const url = "https://api.kit.com/v4/subscribers";
-    const result = await axios.post(url, data, {
-      headers: {
-        Authorization: `Bearer ${this.apiKey}`,
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
+    const url = `${this.baseUrl}/subscribers`;
+    const result = await axios.post(url, data, this.headers);
     return result.data;
   }
 }
--- packages/backend/src/email/email.types.ts
+++ packages/backend/src/email/email.types.ts
@@ -1,32 +1,12 @@
 import { z } from "zod";
+import { SubscriberStateSchema } from "@core/types/email/email.types";
 
-const SubscriberState = z
-  .enum(["active", "bounced", "cancelled", "complained", "inactive"])
-  .nullable()
-  .optional()
-  .default("active");
-
-export const RequestBody_CreateSubscriberSchema = z.object({
-  email_address: z.string().email("Valid email required"),
-  first_name: z.string().nullable().optional(),
-  state: SubscriberState,
-  fields: z.object({
-    "Last name": z.string(),
-    Birthday: z.string(),
-    Source: z.string(),
-    Role: z.string().optional(),
-    Company: z.string().optional(),
-    "Postal code": z.string().optional(),
-    Website: z.string().optional(),
-    "Social media": z.string().optional(),
-    "How did you hear about us?": z.string().optional(),
-    Interests: z.string().optional(),
-    Coupon: z.string().optional(),
-  }),
+export const RequestBody_AddTagToSubscriberSchema = z.object({
+  email_address: z.string().email(),
 });
 
-export type RequestBody_CreateSubscriber = z.infer<
-  typeof RequestBody_CreateSubscriberSchema
+export type RequestBody_AddTagToSubscriber = z.infer<
+  typeof RequestBody_AddTagToSubscriberSchema
 >;
 
 export const Response_UpsertSubscriberSchema = z.object({
@@ -34,7 +14,7 @@ export const Response_UpsertSubscriberSchema = z.object({
     id: z.number().int(),
     first_name: z.string(),
     email_address: z.string().email(),
-    state: SubscriberState,
+    state: SubscriberStateSchema,
     created_at: z.string().datetime(),
     fields: z.object({}).optional(),
   }),
@@ -49,7 +29,7 @@ export const Response_TagSubscriberSchema = z.object({
     id: z.number().int(),
     first_name: z.string(),
     email_address: z.string().email(),
-    state: SubscriberState,
+    state: SubscriberStateSchema,
     created_at: z.string().datetime(),
     tagged_at: z.string().datetime(),
     fields: z.object({}).optional(),
