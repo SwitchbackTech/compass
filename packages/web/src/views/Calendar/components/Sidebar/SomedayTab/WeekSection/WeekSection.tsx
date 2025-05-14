aba5fdac944fd4b2577c5a06b49903da45c5380d
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-05-14 23:39:00 +0530

    feat(web): Change labels for some day events. (#415)
    Co-authored-by: Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
    
    Co-authored-by: Muhammed Aldulaimi <muhammed.aldulaimi98@gmail.com>

--- packages/web/src/views/Calendar/components/Sidebar/SomedayTab/WeekSection/WeekSection.tsx
+++ packages/web/src/views/Calendar/components/Sidebar/SomedayTab/WeekSection/WeekSection.tsx
@@ -26,6 +26,23 @@ export const WeekSection: FC<Props> = ({
   weekLabel,
   gridRefs,
 }) => {
+  const isCurrentWeek = (label: String): boolean => {
+    const [start, end] = label.split(" - ");
+    const [month, startDay] = start.split(".").map(Number);
+    const endDay = Number(end);
+    const now = new Date();
+    const year = now.getFullYear();
+
+    const startDate = new Date(year, month - 1, startDay);
+    const endDate = new Date(year, month - 1, endDay);
+
+    now.setHours(0, 0, 0, 0);
+    startDate.setHours(0, 0, 0, 0);
+    endDate.setHours(0, 0, 0, 0);
+
+    return now >= startDate && now <= endDate;
+  };
+
   return (
     <SidebarSection>
       <SidebarHeader
@@ -33,7 +50,7 @@ export const WeekSection: FC<Props> = ({
         justifyContent={JustifyContent.SPACE_BETWEEN}
       >
         <Text role="heading" size="xl">
-          {weekLabel}
+          {isCurrentWeek(weekLabel) ? "This Week" : weekLabel}
         </Text>
       </SidebarHeader>
 
