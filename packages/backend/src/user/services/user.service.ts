04d1f70ac4e4c6f1f2e9dd350dbcd79842eb9301
Tyler Dane <tyler@switchback.tech>
2025-05-17 16:41:34 -0500

    🧹 Chore: remove redundant log in user service

--- packages/backend/src/user/services/user.service.ts
+++ packages/backend/src/user/services/user.service.ts
@@ -131,7 +131,6 @@ class UserService {
         "Did not tag subscriber due to missing EMAILER_ ENV value(s)",
       );
     } else {
-      logger.debug("Adding tag to subscriber");
       const subscriber = mapCompassUserToEmailSubscriber(cUser);
       await EmailService.addTagToSubscriber(subscriber, ENV.EMAILER_TAG_ID);
     }
