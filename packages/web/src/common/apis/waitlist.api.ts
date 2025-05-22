d0e698566c0f3a2a9fa2dd04841e129915b24bbc
Tyler Dane <tyler@switchback.tech>
2025-05-22 05:35:41 -0500

    feat: implement waitlist status check flow with new UI components

--- packages/web/src/common/apis/waitlist.api.ts
+++ packages/web/src/common/apis/waitlist.api.ts
@@ -2,7 +2,10 @@ import { CompassApi } from "./compass.api";
 
 export const WaitlistApi = {
   async getWaitlistStatus(email: string) {
-    const res = await CompassApi.get(`/waitlist`, { params: { email } });
-    return res.data;
+    const { data } = await CompassApi.get<{
+      isOnWaitlist: boolean;
+      isInvited: boolean;
+    }>(`/waitlist`, { params: { email } });
+    return data;
   },
 };
