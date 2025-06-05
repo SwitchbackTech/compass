3cdcdb8db059e1183ae64820c94265302b4781e1
Tyler Dane <tyler@switchback.tech>
2025-06-05 06:51:04 -0500

    refactor: update test event dates to use fixed Thursday 11am-12pm time slot
    not sure this is the ideal way to handle this, but moving forward with it for now in order to get tests to pass on `main`

--- packages/web/src/__tests__/Calendar/calendar.render.test.utils.ts
+++ packages/web/src/__tests__/Calendar/calendar.render.test.utils.ts
@@ -128,8 +128,22 @@ export const createInitialState = (
 };
 
 export const freshenEventStartEndDate = (event: Schema_Event): Schema_Event => {
-  const newStartDate = dayjs(new Date()).add(1, "day").format();
-  const newEndDate = dayjs(new Date()).add(3, "day").format();
+  // Set event to start on the current week's Thursday at 11am and end at 12pm (timed event)
+  const now = dayjs();
+  const startOfWeek = now.startOf("week"); // Sunday
+  const thursday = startOfWeek.add(4, "day"); // Thursday
+  const newStartDate = thursday
+    .hour(11)
+    .minute(0)
+    .second(0)
+    .millisecond(0)
+    .format();
+  const newEndDate = thursday
+    .hour(12)
+    .minute(0)
+    .second(0)
+    .millisecond(0)
+    .format();
   return { ...event, startDate: newStartDate, endDate: newEndDate };
 };
 
