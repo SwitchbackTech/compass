e1abee340f85d56fc84fec80a9bbed403028952a
Muhammed Al-Dulaimi <muhammed.aldulaimi98@gmail.com>
2025-06-30 05:07:25 +0300

    feat(web): switch migrate buttons to phosphor icons, capitalize 'Ctrl' key for better UI

--- packages/web/src/views/Forms/EventForm/MigrateBackwardButton.tsx
+++ packages/web/src/views/Forms/EventForm/MigrateBackwardButton.tsx
@@ -1,15 +1,8 @@
 import React from "react";
-import styled from "styled-components";
+import { ArrowLeft } from "@phosphor-icons/react";
 import { getMetaKey } from "@web/common/utils/shortcut.util";
 import { Text } from "@web/components/Text";
 import TooltipIconButton from "@web/components/TooltipIconButton/TooltipIconButton";
-
-const StyledMigrateBackwardButton = styled.div`
-  font-size: 25px;
-  &:hover {
-    cursor: pointer;
-  }
-`;
 
 export const MigrateBackwardButton = ({
   onClick,
@@ -20,18 +13,14 @@ export const MigrateBackwardButton = ({
 }) => {
   return (
     <TooltipIconButton
-      component={
-        <StyledMigrateBackwardButton id="migrate-backward-button" role="button">
-          {"<"}
-        </StyledMigrateBackwardButton>
-      }
+      icon={<ArrowLeft />}
       buttonProps={{ "aria-label": tooltipText }}
       tooltipProps={{
         description: tooltipText,
         onClick,
         shortcut: (
           <Text size="s" style={{ display: "flex", alignItems: "center" }}>
-            Ctrl + {getMetaKey()} + Left
+            CTRL + {getMetaKey()} + Left
           </Text>
         ),
       }}
--- packages/web/src/views/Forms/EventForm/MigrateForwardButton.tsx
+++ packages/web/src/views/Forms/EventForm/MigrateForwardButton.tsx
@@ -1,15 +1,8 @@
 import React from "react";
-import styled from "styled-components";
+import { ArrowRight } from "@phosphor-icons/react";
 import { getMetaKey } from "@web/common/utils/shortcut.util";
 import { Text } from "@web/components/Text";
 import TooltipIconButton from "@web/components/TooltipIconButton/TooltipIconButton";
-
-const StyledMigrateForwardButton = styled.div`
-  font-size: 25px;
-  &:hover {
-    cursor: pointer;
-  }
-`;
 
 export const MigrateForwardButton = ({
   onClick,
@@ -20,18 +13,14 @@ export const MigrateForwardButton = ({
 }) => {
   return (
     <TooltipIconButton
-      component={
-        <StyledMigrateForwardButton id="migrate-forward-button" role="button">
-          {">"}
-        </StyledMigrateForwardButton>
-      }
+      icon={<ArrowRight />}
       buttonProps={{ "aria-label": tooltipText }}
       tooltipProps={{
         description: tooltipText,
         onClick,
         shortcut: (
           <Text size="s" style={{ display: "flex", alignItems: "center" }}>
-            Ctrl + {getMetaKey()} + Right
+            CTRL + {getMetaKey()} + Right
           </Text>
         ),
       }}
