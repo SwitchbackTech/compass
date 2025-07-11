7b5af8fca76b858094c76c3940b07ae2b85b9204
Muhammed Al-Dulaimi <muhammed.aldulaimi98@gmail.com>
2025-07-11 18:23:14 +0300

    fix(web): Duplicate shortcut tooltip using wrong label

--- packages/web/src/views/Forms/EventForm/DuplicateButton.tsx
+++ packages/web/src/views/Forms/EventForm/DuplicateButton.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Copy } from "@phosphor-icons/react";
+import { getMetaKey } from "@web/common/utils/shortcut.util";
+import { Text } from "@web/components/Text";
 import TooltipIconButton from "@web/components/TooltipIconButton/TooltipIconButton";
 
 export const DuplicateButton = ({ onClick }: { onClick: () => void }) => {
@@ -8,7 +10,11 @@ export const DuplicateButton = ({ onClick }: { onClick: () => void }) => {
       icon={<Copy />}
       buttonProps={{ "aria-label": "Duplicate Event [Meta+D]" }}
       tooltipProps={{
-        shortcut: "Meta+D",
+        shortcut: (
+          <Text size="s" style={{ display: "flex", alignItems: "center" }}>
+            {getMetaKey()} + D
+          </Text>
+        ),
         description: "Duplicate Event",
         onClick,
       }}
