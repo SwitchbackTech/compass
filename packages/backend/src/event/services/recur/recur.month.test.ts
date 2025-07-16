3fdfa62cfa758e24b890b69daa14797aa4678ada
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-07-16 17:41:13 +0100

    :sparkles: fix(gcal-series): update  and  logic

--- packages/backend/src/event/services/recur/recur.month.test.ts
+++ packages/backend/src/event/services/recur/recur.month.test.ts
@@ -12,13 +12,13 @@ describe("Event Recurrence: Month", () => {
       },
     } as Schema_Event_Core);
 
-    expect(events[0].startDate).toBe("2023-10-01");
-    expect(events[0].endDate).toBe("2023-10-31");
-    expect(events[1].startDate).toBe("2023-11-01");
-    expect(events[1].endDate).toBe("2023-11-30");
-    expect(events[2].startDate).toBe("2023-12-01");
-    expect(events[2].endDate).toBe("2023-12-31");
-    expect(events[3].startDate).toBe("2024-01-01");
-    expect(events[3].endDate).toBe("2024-01-31");
+    expect(events[0]?.startDate).toBe("2023-10-01");
+    expect(events[0]?.endDate).toBe("2023-10-31");
+    expect(events[1]?.startDate).toBe("2023-11-01");
+    expect(events[1]?.endDate).toBe("2023-11-30");
+    expect(events[2]?.startDate).toBe("2023-12-01");
+    expect(events[2]?.endDate).toBe("2023-12-31");
+    expect(events[3]?.startDate).toBe("2024-01-01");
+    expect(events[3]?.endDate).toBe("2024-01-31");
   });
 });
