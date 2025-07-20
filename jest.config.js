d366b9364057c697a76926e44978f1fb898d63bc
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-07-20 19:02:01 +0100

    :recycle: refactor(jest-init): init test environment for core tests

--- jest.config.js
+++ jest.config.js
@@ -92,7 +92,6 @@ module.exports = {
 
   // An enum that specifies notification mode. Requires { notify: true }
   // notifyMode: "failure-change",
-
   // A preset that is used as a base for Jest's configuration
   preset: "@shelf/jest-mongodb", // https://jestjs.io/docs/mongodb
 
@@ -104,6 +103,10 @@ module.exports = {
       },
       testEnvironment: "node",
       testMatch: ["<rootDir>/packages/core/**/?(*.)+(spec|test).[tj]s?(x)"],
+      setupFiles: ["<rootDir>/packages/core/src/__tests__/core.test.init.ts"],
+      setupFilesAfterEnv: [
+        "<rootDir>/packages/core/src/__tests__/core.test.start.ts",
+      ],
     },
     {
       displayName: "web",
@@ -128,8 +131,12 @@ module.exports = {
           "<rootDir>/packages/web/src/__tests__/__mocks__/svg.stub.js",
         "^uuid$": "uuid",
       },
-      setupFiles: ["<rootDir>/packages/web/src/__tests__/web.test.init.js"],
+      setupFiles: [
+        "<rootDir>/packages/core/src/__tests__/core.test.init.ts",
+        "<rootDir>/packages/web/src/__tests__/web.test.init.js",
+      ],
       setupFilesAfterEnv: [
+        "<rootDir>/packages/core/src/__tests__/core.test.start.ts",
         "<rootDir>/packages/web/src/__tests__/web.test.start.js",
       ],
       testEnvironment: "jsdom",
@@ -162,10 +169,12 @@ module.exports = {
       },
 
       setupFiles: [
+        "<rootDir>/packages/core/src/__tests__/core.test.init.ts",
         "<rootDir>/packages/backend/src/__tests__/backend.test.init.ts",
       ],
       setupFilesAfterEnv: [
-        "<rootDir>/packages/backend/src/__tests__/backend.test.start.js",
+        "<rootDir>/packages/core/src/__tests__/core.test.start.ts",
+        "<rootDir>/packages/backend/src/__tests__/backend.test.start.ts",
       ],
       testEnvironment: "node",
       testMatch: ["<rootDir>/packages/backend/**/?(*.)+(spec|test).[tj]s?(x)"],
