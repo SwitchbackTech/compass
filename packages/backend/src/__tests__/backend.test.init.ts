1f803fa14e7567e1225a3c9388e430591c0c17b7
Tyler Dane <tyler@switchback.tech>
2025-08-29 20:43:43 -0500

    chore(test): organize envs in backend.test.init
    mirror the .env.example for legibility

--- packages/backend/src/__tests__/backend.test.init.ts
+++ packages/backend/src/__tests__/backend.test.init.ts
@@ -1,19 +1,28 @@
 const mongoUri = (global as unknown as { __MONGO_URI__: string }).__MONGO_URI__;
 
+// Backend
 process.env["BASEURL"] = "https://foo.yourdomain.app";
 process.env["CORS"] =
   "https://foo.yourdomain.app,http://localhost:3000,http://localhost:9080";
 process.env["PORT"] = "3000";
+process.env["TOKEN_GCAL_NOTIFICATION"] = "secretToken1";
+process.env["TOKEN_COMPASS_SYNC"] = "secretToken2";
+
+// Database
 process.env["MONGO_URI"] = mongoUri ?? process.env["MONGO_URL"];
 process.env["DB"] = "test-db";
+
+// Google
 process.env["CLIENT_ID"] = "googleClientId";
 process.env["CLIENT_SECRET"] = "googleSecret";
+
+// User Sessions
 process.env["CHANNEL_EXPIRATION_MIN"] = "5";
 process.env["SUPERTOKENS_URI"] = "http://localhost:3000";
 process.env["SUPERTOKENS_KEY"] = "sTKey";
+
+// Email
 process.env["EMAILER_API_SECRET"] = "emailerApiSecret";
 process.env["EMAILER_WAITLIST_TAG_ID"] = "1234567";
 process.env["EMAILER_WAITLIST_INVITE_TAG_ID"] = "7654321";
 process.env["EMAILER_USER_TAG_ID"] = "910111213";
-process.env["TOKEN_GCAL_NOTIFICATION"] = "secretToken1";
-process.env["TOKEN_COMPASS_SYNC"] = "secretToken2";
