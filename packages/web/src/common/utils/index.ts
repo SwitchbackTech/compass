e2870e052da05fe62501418f8316c7a3c569cc10
Tyler Dane <tyler@switchback.tech>
2025-09-28 09:02:48 -0500

    chore(web): delete unused `isSomedayEventFormOpen`

--- packages/web/src/common/utils/index.ts
+++ packages/web/src/common/utils/index.ts
@@ -3,9 +3,6 @@ import {
   ID_SOMEDAY_EVENT_FORM,
 } from "../constants/web.constants";
 
-export const isSomedayEventFormOpen = () =>
-  document.getElementsByName(ID_SOMEDAY_EVENT_FORM).length === 1;
-
 export const isContextMenuOpen = () => {
   const contextMenuItems = document.getElementById(ID_CONTEXT_MENU_ITEMS);
   return !!contextMenuItems;
