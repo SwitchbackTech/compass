629c1cda6a130a8242db62d96fd97db5b42f885e
Tyler Dane <tyler@switchback.tech>
2025-10-04 14:32:00 -0500

    feat(web): update router configuration to use new future features and enhance mock rendering

--- packages/web/src/routers/index.tsx
+++ packages/web/src/routers/index.tsx
@@ -4,32 +4,38 @@ import { ProtectedRoute } from "@web/auth/ProtectedRoute";
 import { UserProvider } from "@web/auth/UserContext";
 import { ROOT_ROUTES } from "@web/common/constants/routes";
 import SocketProvider from "@web/socket/SocketProvider";
-import { LoginView } from "@web/views/Login";
 import { LogoutView } from "@web/views/Logout";
 import { NotFoundView } from "@web/views/NotFound";
 import OnboardingFlow from "@web/views/Onboarding/OnboardingFlow";
 import { RootView } from "@web/views/Root";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: ROOT_ROUTES.ROOT,
+      element: (
+        <ProtectedRoute>
+          <UserProvider>
+            <SocketProvider>
+              <RootView />
+            </SocketProvider>
+          </UserProvider>
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: ROOT_ROUTES.LOGIN,
+      element: <OnboardingFlow />,
+    },
+    { path: ROOT_ROUTES.LOGOUT, element: <LogoutView /> },
+    { path: "*", element: <NotFoundView /> },
+  ],
   {
-    path: ROOT_ROUTES.ROOT,
-    element: (
-      <ProtectedRoute>
-        <UserProvider>
-          <SocketProvider>
-            <RootView />
-          </SocketProvider>
-        </UserProvider>
-      </ProtectedRoute>
-    ),
+    future: {
+      v7_relativeSplatPath: true,
+    },
   },
-  {
-    path: ROOT_ROUTES.LOGIN,
-    element: <OnboardingFlow />,
-  },
-  { path: ROOT_ROUTES.LOGOUT, element: <LogoutView /> },
-  { path: "*", element: <NotFoundView /> },
-]);
+);
 
 export const RootRouter = () => {
   return <RouterProvider router={router} />;
