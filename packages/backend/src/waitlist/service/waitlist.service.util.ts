f41240d2213f7f2819d05b0d608893e328398506
Tyler Dane <tyler@switchback.tech>
2025-10-06 14:13:19 -0500

    feat(backend): add utility function to map waitlist answers to subscriber format

--- packages/backend/src/waitlist/service/waitlist.service.util.ts
+++ packages/backend/src/waitlist/service/waitlist.service.util.ts
@@ -1,3 +1,30 @@
+import { Subscriber } from "@core/types/email/email.types";
+import { Answers_v2 } from "@core/types/waitlist/waitlist.answer.types";
+import { Answers_v1 } from "@core/types/waitlist/waitlist.answer.types";
 import { EmailSchema } from "../types/waitlist.types";
 
 export const getNormalizedEmail = (email: string) => EmailSchema.parse(email);
+
+export const mapWaitlistAnswerToSubscriber = (
+  email: string,
+  answer: Answers_v1 | Answers_v2,
+): Subscriber => {
+  const DEFAULT_BIRTHDAY = "1970-01-01";
+
+  const isV1 = "firstName" in answer && "lastName" in answer;
+
+  const subscriber: Subscriber = {
+    email_address: email,
+    first_name: isV1 ? answer.firstName : null,
+    state: "active",
+    fields: isV1
+      ? {
+          "Last name": (answer as Answers_v1).lastName,
+          Birthday: DEFAULT_BIRTHDAY,
+          Source: answer.source,
+        }
+      : null,
+  };
+
+  return subscriber;
+};
