29ca2870df7d225c7132b4dbb6f7ac8454b89776
Tyler Dane <tyler@switchback.tech>
2025-10-13 18:28:36 -0500

    feat(web): integrate Tailwind CSS for styling and add PostCSS configuration

---
+++ packages/web/tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
