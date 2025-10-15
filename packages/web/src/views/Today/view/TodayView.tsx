50b9c79deabed541c51b24904e471993402d3760
Tyler Dane <tyler@switchback.tech>
2025-10-15 17:05:05 -0500

    feat(web): add feature flag hook and integrate into TodayView

--- packages/web/src/views/Today/view/TodayView.tsx
+++ packages/web/src/views/Today/view/TodayView.tsx
@@ -1,21 +1,33 @@
-import { useFeatureFlagEnabled } from "posthog-js/react";
 import React from "react";
+import { useFeatureFlags } from "@web/common/hooks/useFeatureFlags";
 import { TaskProvider } from "../context/TaskProvider";
 import { TodayViewContent } from "./TodayViewContent";
 
 export function TodayView() {
-  const isPlannerEnabled = useFeatureFlagEnabled("experiment_planner");
-
-  return (
-    <TaskProvider>
-      {!isPlannerEnabled && (
-        <div className="bg-orange/20 border-orange/30 text-white-100 border-b px-4 py-2 text-sm">
+  const { isPlannerEnabled } = useFeatureFlags();
+  if (isPlannerEnabled) {
+    return (
+      <TaskProvider>
+        <TodayViewContent />
+      </TaskProvider>
+    );
+  } else {
+    return (
+      <div className="bg-orange/20 border-orange/30 text-white-100 border-b px-4 py-2 text-sm text-white">
+        <p>
           <strong>Experimental Feature:</strong> This feature is currently in
-          beta. Ask the admin to enable the "experiment_planner" feature flag in
-          PostHog to dismiss this warning.
-        </div>
-      )}
-      <TodayViewContent />
-    </TaskProvider>
-  );
+          beta. Click the flask icon and toggle the "experiment_planner" feature
+          flag to try it out.
+        </p>
+        <p>
+          If you do not see the toggle icon after clicking the flask icon, ask
+          Tyler to add you to the list of beta users.
+        </p>
+        <p>
+          If nothing happens when you click the flask icon, refresh the page and
+          retry.
+        </p>
+      </div>
+    );
+  }
 }
