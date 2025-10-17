fb6c0260ef4e96cb39d50937bb3eb85e89095a03
Tyler Dane <tyler@switchback.tech>
2025-10-17 18:37:37 -0500

    feat(web): enhance accessibility by adding aria attributes to PlusIcon and ShortcutTip components

--- packages/web/src/views/Today/components/Shortcuts/ShortcutTip.tsx
+++ packages/web/src/views/Today/components/Shortcuts/ShortcutTip.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 
-export const ShortcutTip = ({ shortcut }: { shortcut: string }) => {
+interface ShortcutTipProps {
+  shortcut: string;
+  "aria-label"?: string;
+}
+
+export const ShortcutTip = ({
+  shortcut,
+  "aria-label": ariaLabel,
+}: ShortcutTipProps) => {
   return (
-    <span className="rounded bg-gray-400 px-1.5 py-0.5 text-xs text-gray-300">
+    <span
+      className="rounded bg-gray-400 px-1.5 py-0.5 text-xs text-gray-300"
+      aria-label={ariaLabel}
+    >
       {shortcut}
     </span>
   );
