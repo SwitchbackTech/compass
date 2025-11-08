9544816c8a1e39b1c70a929814a21d66d917fb0e
Tyler Dane <tyler@switchback.tech>
2025-11-08 11:21:31 -0500

    test: enhance `useHeaderAnimation` tests with proper timer management

--- packages/web/src/views/Onboarding/steps/events/SomedaySandbox/useHeaderAnimation.test.ts
+++ packages/web/src/views/Onboarding/steps/events/SomedaySandbox/useHeaderAnimation.test.ts
@@ -2,14 +2,25 @@ import { act } from "react";
 import { renderHook } from "@testing-library/react";
 import { useHeaderAnimation } from "./useHeaderAnimation";
 
-it.skip("should set header animation to false after 2.5 seconds", () => {
-  const { result } = renderHook(() => useHeaderAnimation());
-
-  expect(result.current.isHeaderAnimating).toBe(true);
-
-  act(() => {
-    jest.advanceTimersByTime(2500);
+describe("useHeaderAnimation", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
   });
 
-  expect(result.current.isHeaderAnimating).toBe(false);
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  it("should set header animation to false after 2.5 seconds", () => {
+    const { result } = renderHook(() => useHeaderAnimation());
+
+    expect(result.current.isHeaderAnimating).toBe(true);
+
+    act(() => {
+      jest.advanceTimersByTime(2500);
+    });
+
+    expect(result.current.isHeaderAnimating).toBe(false);
+  });
 });
