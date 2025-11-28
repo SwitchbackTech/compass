751398da5e37946e9db08fe44c4d8d3384550b2f
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-11-29 00:42:29 +0100

    :refactor(shortcut-list): remove unused key icon mapping

--- packages/web/src/components/Shortcuts/ShortcutList.tsx
+++ packages/web/src/components/Shortcuts/ShortcutList.tsx
@@ -1,12 +1,6 @@
-import { CommandIcon, ControlIcon } from "@phosphor-icons/react";
 import { Shortcut } from "@web/common/types/global.shortcut.types";
 import { ShortCutLabel } from "@web/common/utils/shortcut/shortcut.util";
 import { ShortcutHint } from "@web/components/Shortcuts/ShortcutHint";
-
-const keyIconMap: Record<string, React.ReactNode> = {
-  Meta: <CommandIcon key="meta" size={14} data-testid="meta-icon" />,
-  Control: <ControlIcon key="control" size={14} data-testid="control-icon" />,
-};
 
 export const ShortcutList = ({ shortcuts }: { shortcuts: Shortcut[] }) => {
   if (!shortcuts.length) return null;
