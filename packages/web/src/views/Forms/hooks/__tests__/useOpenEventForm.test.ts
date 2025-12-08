1ec905fb2b31e48e66dfd279cfb95b7bba006964
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-12-08 14:57:39 +0100

    fix(tests): update import statement for renderHook in useOpenEventForm tests

--- packages/web/src/views/Forms/hooks/__tests__/useOpenEventForm.test.ts
+++ packages/web/src/views/Forms/hooks/__tests__/useOpenEventForm.test.ts
@@ -1,4 +1,5 @@
-import { act, renderHook } from "@testing-library/react";
+import { act } from "react";
+import { renderHook } from "@testing-library/react";
 import { Origin, Priorities } from "@core/constants/core.constants";
 import dayjs from "@core/util/date/dayjs";
 import { getUserId } from "@web/auth/auth.util";
