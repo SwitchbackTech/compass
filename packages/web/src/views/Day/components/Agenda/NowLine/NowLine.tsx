b4df7e7e9fe84ac3def5c368557f268b3fc7b382
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-12-11 13:22:46 +0100

    refactor(NowLine): simplify return statement by removing useMemo

--- packages/web/src/views/Day/components/Agenda/NowLine/NowLine.tsx
+++ packages/web/src/views/Day/components/Agenda/NowLine/NowLine.tsx
@@ -23,23 +23,20 @@ export const NowLine = ({
     return cleanup;
   }, []);
 
-  return useMemo(
-    () => (
-      <div
-        ref={nowLineRef}
-        data-now-marker="true"
-        className={"absolute right-0 left-0 border-t-2 text-yellow-300"}
-        style={{
-          top: `${getNowLinePosition(currentTime)}px`,
-          zIndex,
-        }}
-      >
-        <div className="now-line absolute -top-1 -left-2 h-2 w-4 rounded-full"></div>
-        <div className="pointer-events-none absolute -top-2 left-0 w-16 rounded-sm bg-[#0c0f17] px-1 text-[11px] leading-none font-medium shadow-sm">
-          {getAgendaEventTime(currentTime)}
-        </div>
+  return (
+    <div
+      ref={nowLineRef}
+      data-now-marker="true"
+      className={"absolute right-0 left-0 border-t-2 text-yellow-300"}
+      style={{
+        top: `${getNowLinePosition(currentTime)}px`,
+        zIndex,
+      }}
+    >
+      <div className="now-line absolute -top-1 -left-2 h-2 w-4 rounded-full"></div>
+      <div className="pointer-events-none absolute -top-2 left-0 w-16 rounded-sm bg-[#0c0f17] px-1 text-[11px] leading-none font-medium shadow-sm">
+        {getAgendaEventTime(currentTime)}
       </div>
-    ),
-    [currentTime, zIndex, nowLineRef],
+    </div>
   );
 };
