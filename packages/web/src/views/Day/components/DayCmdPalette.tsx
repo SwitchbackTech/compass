73728211b8193b524006e26b98d0717c4f27d511
Tyler Dane <tyler@switchback.tech>
2026-01-01 19:06:38 -0600

    refactor(tests): remove redundant reminder tests from Calendar.form.test.tsx
    - Deleted tests related to reminder functionality in the Calendar form, including keyboard shortcuts and command palette interactions, to streamline the test suite and focus on essential features.
    - Removed unused import from DayCmdPalette.tsx to clean up the codebase.

--- packages/web/src/views/Day/components/DayCmdPalette.tsx
+++ packages/web/src/views/Day/components/DayCmdPalette.tsx
@@ -5,7 +5,6 @@ import dayjs from "@core/util/date/dayjs";
 import { moreCommandPaletteItems } from "@web/common/constants/more.cmd.constants";
 import { ROOT_ROUTES } from "@web/common/constants/routes";
 import { pressKey } from "@web/common/utils/dom/event-emitter.util";
-import { onEventTargetVisibility } from "@web/common/utils/dom/event-target-visibility.util";
 import {
   openEventFormCreateEvent,
   openEventFormEditEvent,
