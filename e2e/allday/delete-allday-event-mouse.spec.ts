0ac91831f06f8e1dadbc497d7ba2062cd1e6deff
Tyler Dane <tyler@switchback.tech>
2026-01-06 17:21:33 -0600

    test(e2e): adding failing e2e tests (skipped)

---
+++ e2e/allday/delete-allday-event-mouse.spec.ts
@@ -0,0 +1,26 @@
+import { test } from "@playwright/test";
+import {
+  createEventTitle,
+  deleteEventWithMouse,
+  expectAllDayEventMissing,
+  expectAllDayEventVisible,
+  fillTitleAndSaveWithMouse,
+  openAllDayEventFormWithMouse,
+  prepareCalendarPage,
+} from "../utils/event-test-utils";
+
+test.skip("should delete an all-day event using mouse interaction", async ({
+  page,
+}) => {
+  await prepareCalendarPage(page);
+
+  const title = createEventTitle("All-Day Event");
+  await openAllDayEventFormWithMouse(page);
+  await fillTitleAndSaveWithMouse(page, title);
+  await expectAllDayEventVisible(page, title);
+
+  await page.locator("#allDayRow").getByRole("button", { name: title }).click();
+  await deleteEventWithMouse(page);
+
+  await expectAllDayEventMissing(page, title);
+});
---
+++ e2e/someday/update-someday-event-keyboard.spec.ts
@@ -0,0 +1,32 @@
+import { test } from "@playwright/test";
+import {
+  createEventTitle,
+  expectSomedayEventMissing,
+  expectSomedayEventVisible,
+  fillTitleAndSaveWithKeyboard,
+  openEventForEditingWithKeyboard,
+  openSomedayEventFormWithKeyboard,
+  prepareCalendarPage,
+  updateEventTitle,
+} from "../utils/event-test-utils";
+
+test.skip(({ isMobile }) => isMobile, "Someday sidebar is desktop-only.");
+
+test.skip("should update a someday event using keyboard interaction", async ({
+  page,
+}) => {
+  await prepareCalendarPage(page);
+
+  const title = createEventTitle("Someday Event");
+  await openSomedayEventFormWithKeyboard(page);
+  await fillTitleAndSaveWithKeyboard(page, title);
+  await expectSomedayEventVisible(page, title);
+
+  await openEventForEditingWithKeyboard(page, title);
+
+  const updatedTitle = updateEventTitle("Someday Event");
+  await fillTitleAndSaveWithKeyboard(page, updatedTitle);
+
+  await expectSomedayEventVisible(page, updatedTitle);
+  await expectSomedayEventMissing(page, title);
+});
