7e777dc164b8abe8590a2eebcd712f3260c0812f
Tyler Dane <tyler@switchback.tech>
2026-04-16 20:52:10 -0700

    test(web): migrate timed agenda mocks to bun

--- packages/web/src/views/Day/components/Agenda/Events/TimedAgendaEvent/DraggableTimedAgendaEvent.test.tsx
+++ packages/web/src/views/Day/components/Agenda/Events/TimedAgendaEvent/DraggableTimedAgendaEvent.test.tsx
@@ -2,29 +2,102 @@ import { type UseInteractionsReturn } from "@floating-ui/react";
 import { ObjectId } from "bson";
 import "@testing-library/jest-dom";
 import { fireEvent, screen } from "@testing-library/react";
+import { type ReactNode } from "react";
 import { createMockStandaloneEvent } from "@core/util/test/ccal.event.factory";
-import { render } from "@web/__tests__/__mocks__/mock.render";
-import { useEventResizeActions } from "@web/common/hooks/useEventResizeActions";
-import {
-  CursorItem,
-  useFloatingNodeIdAtCursor,
-} from "@web/common/hooks/useOpenAtCursor";
 import { type Schema_GridEvent } from "@web/common/types/web.event.types";
 import { gridEventDefaultPosition } from "@web/common/utils/event/event.util";
-import { DraggableTimedAgendaEvent } from "@web/views/Day/components/Agenda/Events/TimedAgendaEvent/DraggableTimedAgendaEvent";
-import { useOpenAgendaEventPreview } from "@web/views/Day/hooks/events/useOpenAgendaEventPreview";
-import { useOpenEventContextMenu } from "@web/views/Day/hooks/events/useOpenEventContextMenu";
-import {
-  getAgendaEventPosition,
-  getEventHeight,
-} from "@web/views/Day/util/agenda/agenda.util";
+import { beforeEach, describe, expect, it, mock } from "bun:test";
 
-// Mock the hooks
-jest.mock("@web/views/Day/hooks/events/useOpenAgendaEventPreview");
-jest.mock("@web/views/Day/hooks/events/useOpenEventContextMenu");
-jest.mock("@web/common/hooks/useOpenAtCursor");
-jest.mock("@web/common/hooks/useEventResizeActions");
-jest.mock("@web/views/Day/util/agenda/agenda.util");
+const CursorItem = {
+  EventForm: "EventForm",
+  EventPreview: "EventPreview",
+  EventContextMenu: "EventContextMenu",
+} as const;
+
+const mockGetAgendaEventPosition = mock();
+const mockGetEventHeight = mock();
+const mockOpenAgendaEventPreview = mock();
+const mockOpenEventContextMenu = mock();
+const mockOnResize = mock();
+const mockOnResizeStart = mock();
+const mockOnResizeStop = mock();
+const mockRecipeInit = mock(() => ({}));
+const mockSuperTokensInit = mock();
+const mockUseEventResizeActions = mock();
+const mockUseFloatingNodeIdAtCursor = mock();
+const mockUseOpenAgendaEventPreview = mock();
+const mockUseOpenEventContextMenu = mock();
+
+mock.module("supertokens-web-js", () => ({
+  default: {
+    init: mockSuperTokensInit,
+  },
+}));
+
+mock.module("supertokens-web-js/recipe/emailpassword", () => ({
+  default: {
+    init: mockRecipeInit,
+  },
+}));
+
+mock.module("supertokens-web-js/recipe/emailverification", () => ({
+  default: {
+    init: mockRecipeInit,
+  },
+}));
+
+mock.module("supertokens-web-js/recipe/thirdparty", () => ({
+  default: {
+    init: mockRecipeInit,
+  },
+}));
+
+mock.module("supertokens-web-js/recipe/session", () => ({
+  attemptRefreshingSession: mock(),
+  default: {
+    attemptRefreshingSession: mock(),
+    doesSessionExist: mock().mockResolvedValue(true),
+    getAccessToken: mock().mockResolvedValue("mock-access-token"),
+    getAccessTokenPayloadSecurely: mock().mockResolvedValue({}),
+    getInvalidClaimsFromResponse: mock().mockResolvedValue([]),
+    getUserId: mock().mockResolvedValue("mock-user-id"),
+    init: mockRecipeInit,
+    signOut: mock().mockResolvedValue(undefined),
+    validateClaims: mock().mockResolvedValue([]),
+  },
+}));
+
+mock.module("@react-oauth/google", () => ({
+  GoogleOAuthProvider: ({ children }: { children: ReactNode }) => children,
+  useGoogleLogin: () => mock(),
+}));
+
+mock.module("@web/views/Day/hooks/events/useOpenAgendaEventPreview", () => ({
+  useOpenAgendaEventPreview: mockUseOpenAgendaEventPreview,
+}));
+
+mock.module("@web/views/Day/hooks/events/useOpenEventContextMenu", () => ({
+  useOpenEventContextMenu: mockUseOpenEventContextMenu,
+}));
+
+mock.module("@web/common/hooks/useOpenAtCursor", () => ({
+  CursorItem,
+  useFloatingNodeIdAtCursor: mockUseFloatingNodeIdAtCursor,
+}));
+
+mock.module("@web/common/hooks/useEventResizeActions", () => ({
+  useEventResizeActions: mockUseEventResizeActions,
+}));
+
+mock.module("@web/views/Day/util/agenda/agenda.util", () => ({
+  getAgendaEventPosition: mockGetAgendaEventPosition,
+  getEventHeight: mockGetEventHeight,
+}));
+
+const { render } =
+  require("@web/__tests__/__mocks__/mock.render") as typeof import("@web/__tests__/__mocks__/mock.render");
+const { DraggableTimedAgendaEvent } =
+  require("@web/views/Day/components/Agenda/Events/TimedAgendaEvent/DraggableTimedAgendaEvent") as typeof import("@web/views/Day/components/Agenda/Events/TimedAgendaEvent/DraggableTimedAgendaEvent");
 
 describe("DraggableTimedAgendaEvent", () => {
   const standaloneEvent = createMockStandaloneEvent();
@@ -42,37 +115,38 @@ describe("DraggableTimedAgendaEvent", () => {
   const defaultProps = {
     bounds: document.createElement("div"),
     interactions: {
-      getReferenceProps: jest.fn((props) => props),
-      getFloatingProps: jest.fn(),
-      getItemProps: jest.fn(),
+      getReferenceProps: mock((props) => props),
+      getFloatingProps: mock(),
+      getItemProps: mock(),
     } as unknown as UseInteractionsReturn,
     isDraftEvent: false,
     isNewDraftEvent: false,
     isDisabled: false,
   };
 
-  const mockOpenAgendaEventPreview = jest.fn();
-  const mockOpenEventContextMenu = jest.fn();
-  const mockOnResize = jest.fn();
-  const mockOnResizeStart = jest.fn();
-  const mockOnResizeStop = jest.fn();
-
   beforeEach(() => {
-    jest.clearAllMocks();
-    (useOpenAgendaEventPreview as jest.Mock).mockReturnValue(
-      mockOpenAgendaEventPreview,
-    );
-    (useOpenEventContextMenu as jest.Mock).mockReturnValue(
-      mockOpenEventContextMenu,
-    );
-    (useFloatingNodeIdAtCursor as jest.Mock).mockReturnValue(null);
-    (useEventResizeActions as jest.Mock).mockReturnValue({
+    mockGetAgendaEventPosition.mockClear();
+    mockGetEventHeight.mockClear();
+    mockOpenAgendaEventPreview.mockClear();
+    mockOpenEventContextMenu.mockClear();
+    mockOnResize.mockClear();
+    mockOnResizeStart.mockClear();
+    mockOnResizeStop.mockClear();
+    mockUseEventResizeActions.mockClear();
+    mockUseFloatingNodeIdAtCursor.mockClear();
+    mockUseOpenAgendaEventPreview.mockClear();
+    mockUseOpenEventContextMenu.mockClear();
+
+    mockUseOpenAgendaEventPreview.mockReturnValue(mockOpenAgendaEventPreview);
+    mockUseOpenEventContextMenu.mockReturnValue(mockOpenEventContextMenu);
+    mockUseFloatingNodeIdAtCursor.mockReturnValue(null);
+    mockUseEventResizeActions.mockReturnValue({
       onResize: mockOnResize,
       onResizeStart: mockOnResizeStart,
       onResizeStop: mockOnResizeStop,
     });
-    (getAgendaEventPosition as jest.Mock).mockReturnValue(100);
-    (getEventHeight as jest.Mock).mockReturnValue(50);
+    mockGetAgendaEventPosition.mockReturnValue(100);
+    mockGetEventHeight.mockReturnValue(50);
   });
 
   it("should render correctly", () => {
@@ -127,9 +201,7 @@ describe("DraggableTimedAgendaEvent", () => {
   });
 
   it("should not call interactions when event form is open", () => {
-    (useFloatingNodeIdAtCursor as jest.Mock).mockReturnValue(
-      CursorItem.EventForm,
-    );
+    mockUseFloatingNodeIdAtCursor.mockReturnValue(CursorItem.EventForm);
 
     render(<DraggableTimedAgendaEvent event={baseEvent} {...defaultProps} />);
 
