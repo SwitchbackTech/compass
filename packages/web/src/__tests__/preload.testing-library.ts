33334228c9f5486ee3e8dbfe9a4bb32bf33506da
Tyler Dane <tyler@switchback.tech>
2026-04-16 19:42:14 -0700

    test: add bun test-globals to web

--- packages/web/src/__tests__/preload.testing-library.ts
+++ packages/web/src/__tests__/preload.testing-library.ts
@@ -1,3 +1,4 @@
+/// <reference types="bun-types/test-globals" />
 import { afterEach, expect } from "bun:test";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
