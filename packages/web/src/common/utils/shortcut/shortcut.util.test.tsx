fd6b7e5465a3fedcefa4ce7187d096c37eba5047
Tyler Dane <tyler@switchback.tech>
2026-04-17 11:04:07 -0700

    test(web): convert shortcut util mocks

--- packages/web/src/common/utils/shortcut/shortcut.util.test.tsx
+++ packages/web/src/common/utils/shortcut/shortcut.util.test.tsx
@@ -1,3 +1,4 @@
+import { beforeEach, describe, expect, it, mock, vi } from "bun:test";
 import "@testing-library/jest-dom";
 import { screen } from "@testing-library/react";
 import { render } from "@web/__tests__/__mocks__/mock.render";
@@ -12,7 +13,7 @@ import {
 
 describe("shortcut.util", () => {
   beforeEach(() => {
-    jest.clearAllMocks();
+    vi.clearAllMocks();
   });
 
   describe("expandModInShortcutDisplay", () => {
