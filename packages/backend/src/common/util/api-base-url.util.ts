29fc058a2809ec139aa5fe39b3660d1822dddca9
Ugur Armagan <ugurarmagan93@gmail.com>
2026-04-24 09:57:32 -0500

    feat(backend): add google webhook url helper

--- packages/backend/src/common/util/api-base-url.util.ts
+++ packages/backend/src/common/util/api-base-url.util.ts
@@ -3,3 +3,7 @@ import { ENV } from "@backend/common/constants/env.constants";
 export function getApiBaseURL(): string {
   return ENV.BASEURL;
 }
+
+export function getGcalWebhookBaseURL(): string {
+  return ENV.GCAL_WEBHOOK_BASEURL ?? ENV.BASEURL;
+}
