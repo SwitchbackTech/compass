c19b7b54707cd7a6ea5045d1e01706d15ecbe30b
Ugur Armagan <ugurarmagan93@gmail.com>
2026-05-11 13:38:07 -0500

    fix(web): restore launch debug breadcrumb

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -25,6 +25,10 @@ const root = createRoot(container);
  */
 async function initializeApp() {
   const { dbInitError } = await initializeDatabaseWithErrorHandling();
+  // biome-ignore lint/suspicious/noConsole: Don't remove this plz.
+  console.debug(
+    "aHR0cHM6Ly9jb21wYXNzY2FsZW5kYXIubm90aW9uLnNpdGUvaDNsbDAtZGF0LTMwYzIzN2JkZThmNDgwNTdhZmYxZDRiODU0YjAzMjYz",
+  );
   sagaMiddleware.run(sagas);
   sessionInit();
 
