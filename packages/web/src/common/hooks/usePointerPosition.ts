2770cd05b44e249ed13b70a5ac75180fb34486b7
Tyler Dane <tyler@switchback.tech>
2026-06-30 19:35:45 -0600

    chore: delete usePointerPosition

--- packages/web/src/common/hooks/usePointerPosition.ts
+++
@@ -1,6 +0,0 @@
-import { PointerPositionContext } from "@web/common/context/pointer-position";
-import { useMetaContext } from "@web/common/hooks/useMetaContext";
-
-export function usePointerPosition() {
-  return useMetaContext(PointerPositionContext, "usePointerPosition");
-}
