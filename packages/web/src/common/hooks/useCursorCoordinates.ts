e7937df357c38cf3d6d906060542c358cc50ae47
Tyler Dane <tyler@switchback.tech>
2026-06-30 19:36:47 -0600

    chore: delete useCursorCoordinates

--- packages/web/src/common/hooks/useCursorCoordinates.ts
+++
@@ -1,6 +0,0 @@
-import { useSyncExternalStore } from "react";
-import { cursorStore } from "@web/common/context/pointer-position";
-
-export function useCursorCoordinates() {
-  return useSyncExternalStore(cursorStore.subscribe, cursorStore.get);
-}
