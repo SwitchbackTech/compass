c21547d5085282f1fb6717bb54180b3d4da136fb
Tyler Dane <tyler@switchback.tech>
2026-06-30 19:38:51 -0600

    chore: move useMetaContext

--- packages/web/src/common/refs/useCompassRefs.ts
+++ packages/web/src/common/refs/useCompassRefs.ts
@@ -1,5 +1,5 @@
-import { useMetaContext } from "@web/common/hooks/useMetaContext";
 import { CompassRefsContext } from "@web/common/refs/compass-refs";
+import { useMetaContext } from "@web/common/refs/useMetaContext";
 
 export function useCompassRefs() {
   return useMetaContext(CompassRefsContext, "useCompassRefs");
